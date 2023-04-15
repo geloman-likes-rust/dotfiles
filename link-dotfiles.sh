60dda64c37481ada19d0dde92470f905930bcf98
Gelo <geloman-likes-rust@users.noreply.github.com>
2023-04-15 14:53:59 +0800

    Create symbolic links script

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 dotfiles=(
-  .config
   .bash_aliases
   .tmux.conf
 )
 
 for dotfile in "${dotfiles[@]}"; do
   ln -s ~/".dotfiles/$dotfile" ~/"$dotfile"
+done
+
+configs=(
+  nvim
+  synth-shell
+  neofetch
+)
+
+for config in "${configs[@]}"; do
+  ln -s ~/".dotfiles/.config/$config" ~/".config/$config"
 done
