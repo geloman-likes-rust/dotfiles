0a1a01a95e8f875435373eacd29fb50c8f76c189
geloman-likes-rust <geloman-likes-rust@users.noreply.github.com>
2023-04-27 23:15:50 +0800

    Remove bash load files

--- .bash_load_files.sh
+++
@@ -1,5 +0,0 @@
-##-----------------------------------------------------
-## synth-shell-prompt.sh
-if [ -f ~/.config/synth-shell/synth-shell-prompt.sh ] && [ -n "$( echo $- | grep i )" ]; then
-	source ~/.config/synth-shell/synth-shell-prompt.sh
-fi
