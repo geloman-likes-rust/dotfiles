0585f83252a3032f918d10f05ab254dca42d7701
geloman-likes-rust <geloman-likes-rust@users.noreply.github.com>
2023-04-28 15:17:50 +0800

    Add plugin: lualine

---
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'onedark',
+      -- section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
+      -- component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
+      symbols = {
+        -- unix = '', -- e712
+        -- unix = '',  -- e70f
+        -- unix = '',  -- e711
+      }
+    },
+  },
+}
