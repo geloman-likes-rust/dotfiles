1f7366249450c621b76f2e25b5bf94ae651029fb
geloman-likes-rust <geloman-likes-rust@users.noreply.github.com>
2023-04-28 21:26:08 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+## NEOVIM STABLE - LATEST
+#---------------------------------------------------------------------
 install_neovim() {
 	curl -JLO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 	tar xzvf nvim-linux64.tar.gz
@@ -8,4 +11,18 @@ install_neovim() {
 	ln -s ~/.local/bin/nvim-linux64/bin/nvim ~/.local/bin
 }
 
+## RUSTUP, CARGO, RUSTC
+#---------------------------------------------------------------------
+install_rust() {
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+## NODE VERSION MANAGER
+#---------------------------------------------------------------------
+install_nvm() {
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+}
+
 install_neovim
+install_rust
+install_nvm
