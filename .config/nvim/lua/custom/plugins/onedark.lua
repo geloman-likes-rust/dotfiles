6e06ed1e2df01ac274c4e635ab79642cdf9fa33c
geloman-likes-rust <geloman-likes-rust@users.noreply.github.com>
2023-04-28 15:17:21 +0800

    Add plugin: onedark

---
+++ .config/nvim/lua/custom/plugins/onedark.lua
@@ -0,0 +1,19 @@
+return {
+  'navarasu/onedark.nvim',
+  config = function()
+    require('onedark').setup {
+      style = 'deep',
+      transparent = true,
+      term_colors = true,
+      toggle_style_key = '<leader>t',
+      code_style = {
+        comments = 'italic,bold',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
+      },
+    }
+    vim.cmd.colorscheme 'onedark'
+  end,
+}
