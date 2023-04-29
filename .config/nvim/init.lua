d46eacc9c66e39bdcf87ec4cf116a4d62296f7dc
Gelo <geloman-likes-rust@users.noreply.github.com>
2023-04-29 09:28:36 -0400

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -323,11 +323,11 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  rust_analyzer = {},
-  tsserver = {},
-  cssls = {},
-  emmet_ls = {},
-  html = {},
+  -- rust_analyzer = {},
+  -- tsserver = {},
+  -- cssls = {},
+  -- emmet_ls = {},
+  -- html = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
