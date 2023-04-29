2dc2277e861a3bef89fc5e06ac6e6a82d5040009
Gelo <geloman-likes-rust@users.noreply.github.com>
2023-04-29 10:48:45 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ install_rust() {
 #---------------------------------------------------------------------
 install_nvm() {
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+	nvm install --lts
 }
 
 install_neovim
