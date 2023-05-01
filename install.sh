8d900f6101cb205dd56cd0c30e51abc89218f398
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-01 13:57:05 +0800

    Update intall script

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ install_rust() {
 install_nvm() {
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 	nvm install --lts
+	nvm install-latest-npm
 }
 
 install_neovim
