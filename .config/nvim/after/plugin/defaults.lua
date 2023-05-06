a4a8d7b8c9a8bd178bfb9fa2858b2aac759fca23
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-06 20:23:44 +0800

    Update nvim configs

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -6,6 +6,9 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.smartcase = true
 
 -- line number
 vim.wo.wrap = false
