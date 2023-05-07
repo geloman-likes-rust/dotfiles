4152df2d3f2430d905335ee2bd226ee60cc0f3a7
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-08 01:20:32 +0800

    Update nvimtree configs

--- .config/nvim/lua/custom/plugins/nvimtree.lua
+++ .config/nvim/lua/custom/plugins/nvimtree.lua
@@ -4,6 +4,10 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup {
+      diagnostics = {
+        enable = true
+      }
+    }
   end,
 }
