775b93af25b905275412100cb84be662c6a677e5
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-12 02:38:42 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 
+## TMUX (TERMINAL MULTIPLEXER)
+#---------------------------------------------------------------------
+install_tmux() {
+	echo "Installing miniconda......................................"
+	curl -SL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ~/miniconda.sh
+	chmod +x miniconda.sh
+	bash ~/miniconda.sh -b -p ~/.miniconda && echo "yes" | ~/.miniconda/bin/conda init
+	rm -rdf miniconda.sh
+	echo export PATH="$HOME/.miniconda/bin:$PATH" >> ~/.bashrc
+	source ~/.bashrc
+	echo "Installing tmux..........................................."
+	conda install -y -c conda-forge tmux
+	source ~/.bashrc
+}
+
+## TMUX PLUGIN MANAGER
+#---------------------------------------------------------------------
+install_tmux_tpm() {
+	echo "Installing tmux-plugins/tpm......................................"
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
 ## NEOVIM LATEST VERSION - STABLE
 #---------------------------------------------------------------------
 install_neovim() {
+	echo "Installing neovim......................................"
 	cd ~/.local/bin
 	curl -JLO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 	tar xzvf nvim-linux64.tar.gz
@@ -11,21 +34,17 @@ install_neovim() {
 	ln -s ~/.local/bin/nvim-linux64/bin/nvim ~/.local/bin
 }
 
-## TMUX PLUGIN MANAGER
-#---------------------------------------------------------------------
-install_tmux_tpm() {
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-}
-
 ## RUSTUP, CARGO, RUSTC
 #---------------------------------------------------------------------
 install_rust() {
+	echo "Installing rustup, cargo, rustc......................................"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 ## NODE VERSION MANAGER
 #---------------------------------------------------------------------
 install_nvm() {
+	echo "Installing node-version-manager......................................"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 	nvm install --lts
 	nvm install-latest-npm
@@ -34,16 +53,18 @@ install_nvm() {
 ## EXA LS
 #---------------------------------------------------------------------
 install_exa() {
-  cd ~/.local/bin
-  mkdir exa_ls && cd exa_ls
-  curl -LO https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
-  unzip exa-linux-x86_64-v0.10.1.zip
-  rm -rdf exa-linux-x86_64-v0.10.1.zip
-  ln -s ~/.local/bin/exa_ls/bin/exa ~/.local/bin/
+	echo "Installing exa......................................"
+	cd ~/.local/bin
+	mkdir exa_ls && cd exa_ls
+	curl -LO https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+	unzip exa-linux-x86_64-v0.10.1.zip
+	rm -rdf exa-linux-x86_64-v0.10.1.zip
+	ln -s ~/.local/bin/exa_ls/bin/exa ~/.local/bin/
 }
 
-install_exa
-install_neovim
+install_tmux
 install_tmux_tpm
+install_neovim
+install_exa
 # install_rust
 # install_nvm
