dae996955dbde6595f4cca15404e3707b1dbc2a5
geloman <geloman-likes-rust@users.noreply.github.com>
2023-06-04 03:36:16 +0800

    Add plugin: vim-matchtag

---
+++ .config/nvim/lua/custom/plugins/vim-matchtag.lua
@@ -0,0 +1,7 @@
+return {
+  "leafOfTree/vim-matchtag",
+  config = function()
+    vim.g.vim_matchtag_enable_by_default = 1
+    vim.g.vim_matchtag_files = '*.html,*.ts,*.tsx,*.js,*.jsx,*.vue,*.svelte,*.jsp'
+  end
+}
