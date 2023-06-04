3dd602a54f3306b5f8772afbf000947bf50f9789
geloman <geloman-likes-rust@users.noreply.github.com>
2023-06-05 03:29:44 +0800

    symlink tmux-sessionizer to ~/.local/bin

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -4,6 +4,7 @@ rm -rdf $HOME/.gitconfig
 ln -s $HOME/.dotfiles/.gitconfig $HOME
 ln -s $HOME/.dotfiles/.bash_aliases $HOME
 ln -s $HOME/.dotfiles/.tmux.conf $HOME
+ln -s $HOME/.dotfiles/tmux-sessionizer ~/.local/bin/
 
 for dir in ~/.dotfiles/.config/*; do
   config="$(basename $dir)"
