39f744be0b8432169ccded3bfd0b5f0cd2d44a9c
geloman <geloman-likes-rust@users.noreply.github.com>
2023-06-10 03:03:36 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 BASHRC=$HOME/.bashrc
 SOURCE_SYNTHSHELL='if [ -f ~/.config/synth-shell/synth-shell-prompt.sh ] && [ -n "$( echo $- | grep i )" ]; then\n	source ~/.config/synth-shell/synth-shell-prompt.sh\nfi'
 
-bash ~/.dotfiles/install.sh
-bash ~/.dotfiles/symlink-dotfiles.sh
+~/.dotfiles/install.sh
+~/.dotfiles/symlink-dotfiles.sh
 
 echo -e "\n## opens fzf when ctrl+f key was press and creates a tmux session based on the selected basename" >> $BASHRC
 echo "#-----------------------------------------------------" >> $BASHRC
