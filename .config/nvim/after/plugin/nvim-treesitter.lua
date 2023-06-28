f86cf94b37b7b8b57237918b06680527428ea066
geloman <geloman-likes-rust@users.noreply.github.com>
2023-06-29 01:11:46 +0800

    Add c_sharp

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -1,4 +1,5 @@
-local parsers = { 'c', 'html', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim' }
+local parsers = { 'c', 'html', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim',
+  'c_sharp' }
 require('nvim-treesitter.configs').setup {
   ensure_installed = parsers,
   auto_install = false,
