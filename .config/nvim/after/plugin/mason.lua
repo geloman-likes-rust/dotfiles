b38c8acd4bf6eef6b82757f95d272663315027b8
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-01 01:23:55 +0800

    Fix omnisharp startup error

--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -32,6 +32,7 @@ local language_servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
+      globals = { 'vim' }
     },
   },
 }
@@ -54,3 +55,14 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "Fix startup error by disabling semantic tokens for omnisharp",
+  group = vim.api.nvim_create_augroup("OmnisharpHook", {}),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client.name == "omnisharp" then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+  end,
+})
