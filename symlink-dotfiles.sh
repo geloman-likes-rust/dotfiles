19accfda36975d03e007458562270e224173431d
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-04 05:46:37 +0800

    symlink setbg script to .local/bin

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -5,6 +5,7 @@ ln -s $HOME/.dotfiles/.gitconfig $HOME
 ln -s $HOME/.dotfiles/.bash_aliases $HOME
 ln -s $HOME/.dotfiles/.tmux.conf $HOME
 ln -s $HOME/.dotfiles/tmux-sessionizer ~/.local/bin/
+ln -s $HOME/.dotfiles/setbg ~/.local/bin/
 
 for dir in ~/.dotfiles/.config/*; do
   config="$(basename $dir)"
