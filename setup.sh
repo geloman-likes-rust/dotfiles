e2200617d8c0341016e75ab998a4a2ef8e02f9d1
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-04 07:48:00 +0800

    Source .default_background

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ SOURCE_SYNTHSHELL='if [ -f ~/.config/synth-shell/synth-shell-prompt.sh ] && [ -n
 ~/.dotfiles/install.sh
 ~/.dotfiles/symlink-dotfiles.sh
 
+cat <<'EOF' >> ~/.profile
+. "$HOME/.dotfiles/.default_background"
+EOF
+
 echo -e "\n## opens fzf when ctrl+f key was press and creates a tmux session based on the selected basename" >> $BASHRC
 echo "#-----------------------------------------------------" >> $BASHRC
 cat <<'EOF' >> ~/.bashrc
