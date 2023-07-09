0c9b13e30570d4a0138c738f43d371b684a8a8d2
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-09 18:12:08 +0800

    Symlink randbg & switch_shell

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -6,6 +6,8 @@ ln -s $HOME/.dotfiles/.bash_aliases $HOME
 ln -s $HOME/.dotfiles/.tmux.conf $HOME
 ln -s $HOME/.dotfiles/tmux-sessionizer ~/.local/bin/
 ln -s $HOME/.dotfiles/setbg ~/.local/bin/
+ln -s $HOME/.dotfiles/randbg ~/.local/bin/
+ln -s $HOME/.dotfiles/switch_shell ~/.local/bin/
 
 for dir in ~/.dotfiles/.config/*; do
   config="$(basename $dir)"
