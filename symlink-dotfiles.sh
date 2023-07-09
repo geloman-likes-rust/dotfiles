3bfe15d3ed25b5884ab87219e35ed334be69ca8a
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-09 20:15:44 +0800

    symlink vimrc

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -4,6 +4,7 @@ rm -rdf $HOME/.gitconfig
 ln -s $HOME/.dotfiles/.gitconfig $HOME
 ln -s $HOME/.dotfiles/.bash_aliases $HOME
 ln -s $HOME/.dotfiles/.tmux.conf $HOME
+ln -s $HOME/.dotfiles/.vimrc $HOME
 ln -s $HOME/.dotfiles/tmux-sessionizer ~/.local/bin/
 ln -s $HOME/.dotfiles/setbg ~/.local/bin/
 ln -s $HOME/.dotfiles/randbg ~/.local/bin/
