c1837fce5e2cc0b29602780ffe8658bec4c128a0
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-10 13:00:49 +0800

    symlink all my handy-scripts

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -5,12 +5,12 @@ ln -s $HOME/.dotfiles/.gitconfig $HOME
 ln -s $HOME/.dotfiles/.bash_aliases $HOME
 ln -s $HOME/.dotfiles/.tmux.conf $HOME
 ln -s $HOME/.dotfiles/.vimrc $HOME
-ln -s $HOME/.dotfiles/tmux-sessionizer ~/.local/bin/
-ln -s $HOME/.dotfiles/setbg ~/.local/bin/
-ln -s $HOME/.dotfiles/randbg ~/.local/bin/
-ln -s $HOME/.dotfiles/switch_shell ~/.local/bin/
-ln -s $HOME/.dotfiles/fvi ~/.local/bin/
-ln -s $HOME/.dotfiles/git-branch-previewer ~/.local/bin/gco
+ln -s $HOME/.dotfiles/handy-scripts/tmux-sessionizer ~/.local/bin/
+ln -s $HOME/.dotfiles/handy-scripts/setbg ~/.local/bin/
+ln -s $HOME/.dotfiles/handy-scripts/randbg ~/.local/bin/
+ln -s $HOME/.dotfiles/handy-scripts/switch_shell ~/.local/bin/
+ln -s $HOME/.dotfiles/handy-scripts/fvi ~/.local/bin/
+ln -s $HOME/.dotfiles/handy-scripts/git-branch-previewer ~/.local/bin/gco
 [ -f ~/.local/bin/slnk ] || ln -s $HOME/.dotfiles/symlink-dotfiles.sh ~/.local/bin/slnk
 [ -f ~/.local/bin/ulnk ] ||  ln -s $HOME/.dotfiles/unlink-dotfiles.sh ~/.local/bin/ulnk
 
