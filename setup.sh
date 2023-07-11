347cae49206018f449faeca1f4c01a40f71761f7
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-11 21:40:09 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/geloman-likes-rust/desktop-wallpaper ~/Pictures/wallpaper
+[ -d ~/Pictures/wallpaper ] || git clone https://github.com/geloman-likes-rust/desktop-wallpaper ~/Pictures/wallpaper
 
 cat ~/.dotfiles/.bashrc > ~/.bashrc
 cat ~/.dotfiles/.zshrc > ~/.zshrc
@@ -8,6 +8,6 @@ cat ~/.dotfiles/.zshrc > ~/.zshrc
 [ -f ~/.profile ] && echo -e ". \"\$HOME/.dotfiles/.default_background\"" >> ~/.profile
 
 ~/.dotfiles/install.sh
-~/.dotfiles/symlink-dotfiles.sh
+[ -f ~/.local/bin/slnk ] || ~/.dotfiles/symlink-dotfiles.sh
 
 source ~/.bashrc
