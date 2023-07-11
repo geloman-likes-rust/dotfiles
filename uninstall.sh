32cd2283dbeca8563bd76cc9adaeac6da1f50d19
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-11 21:59:45 +0800

    Create uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ -f ~/.local/bin/ulnk ] && ulnk
+[ -f ~/.local/bin/ulnk ] && unlink ~/.local/bin/ulnk
+[ -f ~/.local/bin/slnk ] && unlink ~/.local/bin/slnk
+[ -d ~/.fzf ] && rm -rdf ~/.fzf
+[ -d ~/.tmux ] && rm -rdf ~/.tmux
+[ -d ~/.vim ] && rm -rdf ~/.vim
+[ -d ~/.nvm ] && rm -rdf ~/.nvm
+[ -d ~/.cargo ] && rm -rdf ~/.cargo
+[ -d ~/.rustup ] && rm -rdf ~/.rustup
+[ -d ~/.miniconda ] && rm -rdf ~/.miniconda
+[ -d ~/.conda ] && rm -rdf ~/.conda
