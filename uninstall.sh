7649b0f5847e59dab4282128110acf8b91af5eb0
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-11 22:48:26 +0800

    Update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 [ -f ~/.local/bin/ulnk ] && ulnk
 [ -f ~/.local/bin/ulnk ] && unlink ~/.local/bin/ulnk
 [ -f ~/.local/bin/slnk ] && unlink ~/.local/bin/slnk
+[ -d ~/.local/share/nvim ] && rm -rdf ~/.local/share/nvim
 [ -d ~/.fzf ] && rm -rdf ~/.fzf
 [ -d ~/.tmux ] && rm -rdf ~/.tmux
 [ -d ~/.vim ] && rm -rdf ~/.vim
