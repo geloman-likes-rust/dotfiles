4be3a8ce94cca1fba74d685fa7a20ef90f769d15
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-11 20:00:44 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,26 +2,10 @@
 
 git clone https://github.com/geloman-likes-rust/desktop-wallpaper ~/Pictures/wallpaper
 
-[ -f ~/.bashrc ] && echo -e export EDITOR="nvim" >> ~/.bashrc
-[ -f ~/.zshrc ] && echo -e export EDITOR="nvim" >> ~/.zshrc
+cat ~/.dotfiles/.bashrc > ~/.bashrc
+cat ~/.dotfiles/.zshrc > ~/.zshrc
+
 [ -f ~/.profile ] && echo -e ". \"\$HOME/.dotfiles/.default_background\"" >> ~/.profile
-
-if [ -f ~/.bashrc ]; then
-cat <<'EOF' >> ~/.bashrc
-
-## synth-shell-prompt.sh
-##-----------------------------------------------------
-[ -f ~/.config/synth-shell/synth-shell-prompt.sh ] && [ -n "$( echo $- | grep i )" ] && source ~/.config/synth-shell/synth-shell-prompt.sh
-
-## opens fzf when ctrl+f key was press and creates a tmux session based on the selected basename
-#-----------------------------------------------------
-bind -x '"\C-f": tmux-sessionizer'
-
-## change background scripts
-bind -x '"\C-b": randbg'
-
-EOF
-fi
 
 ~/.dotfiles/install.sh
 ~/.dotfiles/symlink-dotfiles.sh
