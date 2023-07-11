276b320d7d1821eff81c888815ac770638b4d7de
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-11 18:59:49 +0800

    Update symlink script

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -5,18 +5,16 @@ ln -s $HOME/.dotfiles/.gitconfig $HOME
 ln -s $HOME/.dotfiles/.bash_aliases $HOME
 ln -s $HOME/.dotfiles/.tmux.conf $HOME
 ln -s $HOME/.dotfiles/.vimrc $HOME
+
 [ -d ~/.local/bin ] || mkdir ~/.local && mkdir ~/.local/bin
-ln -s $HOME/.dotfiles/handy-scripts/tmux-sessionizer ~/.local/bin/
-ln -s $HOME/.dotfiles/handy-scripts/setbg ~/.local/bin/
-ln -s $HOME/.dotfiles/handy-scripts/randbg ~/.local/bin/
-ln -s $HOME/.dotfiles/handy-scripts/switch_shell ~/.local/bin/
-ln -s $HOME/.dotfiles/handy-scripts/fvi ~/.local/bin/
-ln -s $HOME/.dotfiles/handy-scripts/git-branch-previewer ~/.local/bin/gco
-ln -s $HOME/.dotfiles/handy-scripts/se ~/.local/bin/
-ln -s $HOME/.dotfiles/handy-scripts/dirty ~/.local/bin/
+for x in ~/.dotfiles/handy-scripts/*; do
+  ln -s $x ~/.local/bin/
+done
+
 [ -f ~/.local/bin/slnk ] || ln -s $HOME/.dotfiles/symlink-dotfiles.sh ~/.local/bin/slnk
 [ -f ~/.local/bin/ulnk ] ||  ln -s $HOME/.dotfiles/unlink-dotfiles.sh ~/.local/bin/ulnk
 
+[ -d ~/.config ] || mkdir ~/.config
 for dir in ~/.dotfiles/.config/*; do
   config="$(basename $dir)"
   [ -d ~/.config/$config ] && rm -rdf ~/.config/$config
