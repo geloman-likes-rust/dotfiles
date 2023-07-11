d8096085e6c9d352bb1c4b4afb2af9dfb6221349
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-11 19:46:47 +0800

    Update unlink script

--- unlink-dotfiles.sh
+++ unlink-dotfiles.sh
@@ -4,16 +4,13 @@ unlink ~/.gitconfig
 unlink ~/.bash_aliases
 unlink ~/.tmux.conf
 unlink ~/.vimrc
-unlink ~/.local/bin/tmux-sessionizer
-unlink ~/.local/bin/setbg
-unlink ~/.local/bin/randbg
-unlink ~/.local/bin/switch_shell
-unlink ~/.local/bin/fvi
-unlink ~/.local/bin/gco
-unlink ~/.local/bin/se
-unlink ~/.local/bin/dirty
+
+for script in ~/.dotfiles/handy-scripts/*; do
+  file=$(basename $script)
+  unlink ~/.local/bin/$file
+done
 
 for dir in ~/.dotfiles/.config/*; do
-  config="$(basename $dir)"
-  unlink ~/.config/"${config}"
+  config=$(basename $dir)
+  unlink ~/.config/$config
 done
