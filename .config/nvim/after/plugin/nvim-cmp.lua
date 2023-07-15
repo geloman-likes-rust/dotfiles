5e5773672883b9f31605ec32ce88c7fc0c65ca46
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-15 18:29:21 +0800

    Create mapping for 'select_next_item' and 'selected_prev_item'

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -11,10 +11,12 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert {
+
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
+
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -24,6 +26,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
+
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
@@ -33,9 +36,32 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
+
+    ['<C-j>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<C-k>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
   },
+
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = 'nvim_lsp', max_item_count = 5 },
+    { name = 'luasnip',  max_item_count = 5 },
   },
+
 }
