e83e6e8cb445ad18740eabf03c89ab5515909d6c
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-21 14:06:49 +0800

    unmap space

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -17,11 +17,6 @@ cmp.setup {
       select = true,
     },
 
-    ['<Space>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
