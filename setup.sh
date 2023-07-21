36fc6ea28013dd13778ebf934d6aad67bf5ed1fc
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-21 20:21:35 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 
 [ -d ~/.personal/wallpaper ] || git clone https://github.com/geloman-likes-rust/desktop-wallpaper ~/.personal/wallpaper
 
-[ -d ~/.local ] || mkdir ~/.local
-[ -d ~/.local/bin ] || mkdir ~/.local/bin
+[ -d ~/.local/bin ] || mkdir ~/.local && mkdir ~/.local/bin
 [ -d ~/.config ] || mkdir ~/.config
 
 [ -f ~/.bashrc ] || cat ~/.dotfiles/.bashrc > ~/.bashrc
