191e12168a5f27dd1b5934794948cae7e20a71a7
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-22 10:18:58 +0800

    Update gitsigns config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,6 +72,20 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      numhl = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 100,
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>lb', require('gitsigns').toggle_current_line_blame,
+          { buffer = bufnr, desc = '[L]ine [B]lame toggle' })
+      end,
     },
   },
 
