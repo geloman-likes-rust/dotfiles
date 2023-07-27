c5a53e3a660af37aaff6c940fe5bd26b8b4fdf50
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-27 19:26:02 +0800

    Customize gitsigns colors

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -24,3 +24,6 @@ require("gitsigns").setup {
       { buffer = bufnr, desc = '[L]ine [B]lame toggle' })
   end,
 }
+
+vim.cmd("highlight! DiffDelete guifg=#FC2947 guibg=#ffeeee")
+vim.cmd("highlight! DiffAdd guifg=#03C988 guibg=#d6ffd6")
