443c25f3e20fd0af6bf7b16658eba1c6776ce504
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-29 14:17:28 +0800

    Open fugitive in vsplit

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -66,7 +66,7 @@ keymap("n", "+", "horizontal resize +3<cr>", { noremap = true, silent = true, de
 
 keymap("n", "<leader>q", ":set hlsearch!<cr>", { noremap = true, silent = true, desc = "toggle highlight search" })
 keymap("n", "<leader>gc", ":Git commit<cr>", { noremap = true, silent = true, desc = "[G]it [C]ommit" })
-keymap("n", "<leader>G", ":Git<cr>", { noremap = true, silent = true, desc = "[G]it fugitive" })
+keymap("n", "<leader>G", ":vertical Git<cr>", { noremap = true, silent = true, desc = "[G]it fugitive" })
 keymap("n", "<leader>gds", ":Gvdiffsplit<cr>", { noremap = true, silent = true, desc = "[G]it [D]iff [S]plit" })
 
 keymap("n", "<leader>e", ":Lex 25<cr>", { noremap = true, silent = true, desc = "toggle Lex" })
