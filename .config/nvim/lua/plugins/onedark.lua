2ff3b84e659103557e5b8c708537029be6a58586
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 07:52:24 +0800

    Update onedark configs

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -1,18 +1,16 @@
 return {
   'navarasu/onedark.nvim',
   priority = 1000,
-  config = function()
-    require('onedark').setup {
-      style = 'deep',
-      transparent = true,
-      term_colors = true,
-      code_style = {
-        comments = 'italic,bold',
-        keywords = 'none',
-        functions = 'none',
-        strings = 'none',
-        variables = 'none'
-      },
-    }
-  end,
+  opts = {
+    style = 'deep',
+    transparent = true,
+    term_colors = true,
+    code_style = {
+      comments = 'italic,bold',
+      keywords = 'none',
+      functions = 'none',
+      strings = 'none',
+      variables = 'none'
+    },
+  }
 }
