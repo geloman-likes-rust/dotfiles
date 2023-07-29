d06c15a7fabf489aacc1d6adeeeac4eb86b51fe6
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 07:50:22 +0800

    Add plugin for lsp configuration: - nvim-lspconfig - mason.nvim - mason-lspconfig.nvim

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,10 @@
+-- LSP Configuration & Plugins
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    -- Automatically install LSPs to stdpath for neovim
+    { 'williamboman/mason.nvim', config = true },
+    'williamboman/mason-lspconfig.nvim',
+
+  },
+}
