5aef8adb016bea4b2407454ef81e700d93402b28
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 07:57:01 +0800

    Update fidget configs

--- .config/nvim/after/plugin/fidget.lua
+++ .config/nvim/after/plugin/fidget.lua
@@ -1,16 +1,4 @@
-require("fidget").setup {
-  text = {
-    spinner = "circle_quarters"
-  },
-  timer = {
-    fidget_decay = 250,
-  },
-  window = {
-    border = "rounded",
-    blend = 0
-  }
-}
-
+-- custom highlights
 vim.cmd("highlight! FidgetTitle guifg=#41a7fc guibg=NONE")
 vim.cmd("highlight! FidgetTask guifg=#93a4c3 guibg=NONE")
 vim.cmd("highlight! FloatBorder guibg=NONE guifg=#455574")
--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,1 +1,16 @@
-return { 'j-hui/fidget.nvim', tag = "legacy" }
+return {
+  'j-hui/fidget.nvim',
+  tag = "legacy",
+  opts = {
+    text = {
+      spinner = "circle_quarters"
+    },
+    timer = {
+      fidget_decay = 250,
+    },
+    window = {
+      border = "rounded",
+      blend = 0
+    }
+  }
+}
