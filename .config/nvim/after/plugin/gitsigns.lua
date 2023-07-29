b5532c899b03eb16d3bb2e017b41c62a250160b7
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-29 13:43:56 +0800

    Create mappings for stage_hunk and undo_stage_hunk

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -20,6 +20,8 @@ require("gitsigns").setup {
       { buffer = bufnr, desc = '[N] go to previous hunk' })
     vim.keymap.set('n', '<leader>n', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[n] go to next hunk' })
     vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+    vim.keymap.set('n', '<leader>ga', require('gitsigns').stage_hunk, { desc = '[G]it [A]dd Hunk' })
+    vim.keymap.set('n', '<leader>gu', require('gitsigns').undo_stage_hunk, { desc = '[G]it [A]dd Hunk' })
     vim.keymap.set('n', '<leader>lb', require('gitsigns').toggle_current_line_blame,
       { buffer = bufnr, desc = '[L]ine [B]lame toggle' })
   end,
