1a65146b2cc0317f55dd759d3a96cd005f50db64
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 07:52:46 +0800

    Add plugin for fuzzy finding files: - telescope.nvim - plenary.nvim - telescope-fzf-native.nvim

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    }
+  },
+}
