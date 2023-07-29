bc70b4fec0d13da39d1fc917ff4ceeb2af6c6669
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 07:44:54 +0800

    Update kanagawa configs

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,18 +1,17 @@
 return {
   "rebelot/kanagawa.nvim",
   priority = 1000,
-  config = function()
-    require("kanagawa").setup { transparent = false,
-      commentStyle = { italic = true, bold = true },
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none"
-            }
+  opts = {
+    transparent = true,
+    commentStyle = { italic = true, bold = true },
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = "none"
           }
         }
       }
     }
-  end
+  }
 }
