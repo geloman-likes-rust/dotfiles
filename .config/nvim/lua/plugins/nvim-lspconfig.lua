7976b5472ee359129208919e4fb953bed38a4542
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 08:43:48 +0800

    set the load priority of nvim-lspconfig to 1000

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,7 @@
 -- LSP Configuration & Plugins
 return {
   'neovim/nvim-lspconfig',
+  priority = 1000,
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
     { 'williamboman/mason.nvim', config = true },
