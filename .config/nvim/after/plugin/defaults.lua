2675835ca2cd1087e99cb1c72985487fa002e346
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 18:09:34 +0800

    Update after/plugin/defaults

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -10,11 +10,6 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
 
--- custom highlights
-vim.cmd("highlight! Search guibg=#d6ffd6")
-vim.cmd("highlight! CurSearch guifg=#d6ffd6 guibg=#03c988")
-vim.cmd("highlight! IncSearch guifg=#d6ffd6 guibg=#03c988")
-
 -- tab space
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
