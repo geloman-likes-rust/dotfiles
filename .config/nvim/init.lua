b07db77c9cafa1932fe0a39c0d3f06ea2afae1ef
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 20:59:46 +0800

    Move neovide config to a separate file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-if vim.g.neovide then
-  vim.o.guifont = "Hack Nerd Font:h16" -- text below applies for VimScript
-  vim.g.neovide_transparency = 0.8
-  vim.g.transparency = 0.8
-  vim.g.neovide_padding_top = 0
-  vim.g.neovide_padding_bottom = 0
-  vim.g.neovide_padding_right = 0
-  vim.g.neovide_padding_left = 0
-  vim.g.neovide_background_color = "#1A212E"
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_confirm_quit = false
-end
-
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
