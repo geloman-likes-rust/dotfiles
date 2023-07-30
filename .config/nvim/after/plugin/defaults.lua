fae25c67134023b0ac7b3caab2d5256ac490e245
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 08:32:38 +0800

    Update defaults

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -64,12 +64,10 @@ keymap("n", "-", ":vertical resize -5<cr>", { noremap = true, silent = true, des
 keymap("n", "_", "horizontal resize -3<cr>", { noremap = true, silent = true, desc = "[_] (-) horizontal resize" })
 keymap("n", "+", "horizontal resize +3<cr>", { noremap = true, silent = true, desc = "[+] (+) horizontal resize" })
 
+-- toggle highlight search
 keymap("n", "<leader>q", ":set hlsearch!<cr>", { noremap = true, silent = true, desc = "toggle highlight search" })
-keymap("n", "<leader>gc", ":Git commit<cr>", { noremap = true, silent = true, desc = "[G]it [C]ommit" })
-keymap("n", "<leader>gp", ":Git push<cr>", { noremap = true, desc = "[G]it [P]ush" })
-keymap("n", "<leader>G", ":vertical Git<cr>", { noremap = true, silent = true, desc = "[G]it fugitive" })
-keymap("n", "<leader>gds", ":Gvdiffsplit<cr>", { noremap = true, silent = true, desc = "[G]it [D]iff [S]plit" })
 
+-- toggle Lexplore
 keymap("n", "<leader>e", ":Lex 25<cr>", { noremap = true, silent = true, desc = "toggle Lex 25" })
 
 -- close current buffer
