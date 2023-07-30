e84ed2e33b873cbeb6e64905c361055395019be6
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 10:46:41 +0800

    Make setup.sh script posix compliant

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 [ -d ~/.personal/wallpaper ] || git clone https://github.com/geloman-likes-rust/desktop-wallpaper ~/.personal/wallpaper
 
-[ -d ~/.local ] || mkdir ~/.local && [ -d ~/.local/bin ] || mkdir ~/.local/bin
+[ -d ~/.local ] || mkdir ~/.local
+[ -d ~/.local/bin ] || mkdir ~/.local/bin
 [ -d ~/.config ] || mkdir ~/.config
 
 cat ~/.dotfiles/.bashrc > ~/.bashrc
