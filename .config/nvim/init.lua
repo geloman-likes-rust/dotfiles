326a387a67cc5605c25e9eee245dd5f11c88e95e
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 18:08:53 +0800

    Create custom highlights for hlsearch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
+-- custom highlights
+vim.cmd("highlight! Search guibg=#d6ffd6")
+vim.cmd("highlight! CurSearch guifg=#d6ffd6 guibg=#03c988")
+vim.cmd("highlight! IncSearch guifg=#d6ffd6 guibg=#03c988")
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
