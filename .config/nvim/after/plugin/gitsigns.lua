c47f8dbb630fe331423263758d9c46037e1c4344
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 15:46:33 +0800

    Update gitsigns custom keymaps

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -2,11 +2,11 @@
 vim.keymap.set('n', '<leader>N', require('gitsigns').prev_hunk,
   { buffer = bufnr, desc = '[N] go to previous hunk' })
 vim.keymap.set('n', '<leader>n', require('gitsigns').next_hunk, { desc = '[n] go to next hunk' })
-vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { desc = '[P]review [H]unk' })
-vim.keymap.set('n', '<leader>ga', require('gitsigns').stage_hunk, { desc = '[G]it [A]dd Hunk' })
-vim.keymap.set('n', '<leader>gu', require('gitsigns').undo_stage_hunk, { desc = '[G]it [A]dd Hunk' })
-vim.keymap.set('n', '<leader>lb', require('gitsigns').toggle_current_line_blame,
-  { buffer = bufnr, desc = '[L]ine [B]lame toggle' })
+vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { desc = '[p]review [h]unk' })
+vim.keymap.set('n', 'ga', require('gitsigns').stage_hunk, { desc = '[G]it [A]dd Hunk' })
+vim.keymap.set('n', '<leader>ga', require('gitsigns').stage_buffer, { desc = '[G]it [A]dd All Hunks' })
+vim.keymap.set('n', '<leader>gu', require('gitsigns').undo_stage_hunk, { desc = '[G]it [U]nstage Last Staged' })
+vim.keymap.set('n', '<leader>lb', require('gitsigns').toggle_current_line_blame, { desc = '[L]ine [B]lame toggle' })
 
 -- custom highlights
 vim.cmd("highlight! DiffDelete guifg=#FC2947 guibg=#ffeeee")
