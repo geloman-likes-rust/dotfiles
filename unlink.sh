3b906b5436048a7754071bafe28c1e65c87ead69
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 10:41:41 +0800

    Make unlink.sh posix compliant script

--- unlink.sh
+++ unlink.sh
@@ -8,11 +8,11 @@ unlink ~/.bash_prompt
 unlink ~/.zsh_prompt
 
 for script in ~/.dotfiles/handy-scripts/*; do
-  file=$(basename $script)
-  unlink ~/.local/bin/$file
+  file=$(basename "$script")
+  unlink ~/.local/bin/"$file"
 done
 
 for dir in ~/.dotfiles/.config/*; do
-  config=$(basename $dir)
-  unlink ~/.config/$config
+  config=$(basename "$dir")
+  unlink ~/.config/"$config"
 done
