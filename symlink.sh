744a73699ba28fac40737243e8121a566b6efca5
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 10:40:53 +0800

    Make symlink.sh posix compliant script

--- symlink.sh
+++ symlink.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-[ -f $HOME/.gitconfig ] && rm -rdf $HOME/.gitconfig
-ln -s $HOME/.dotfiles/.bash_prompt $HOME
-ln -s $HOME/.dotfiles/.zsh_prompt $HOME
-ln -s $HOME/.dotfiles/.gitconfig $HOME
-ln -s $HOME/.dotfiles/.aliasrc $HOME
-ln -s $HOME/.dotfiles/.tmux.conf $HOME
-[ -f ~/.vimrc ] || ln -s $HOME/.dotfiles/.vimrc $HOME
+[ -f "$HOME"/.gitconfig ] && rm -rdf "$HOME"/.gitconfig
+ln -s "$HOME"/.dotfiles/.bash_prompt "$HOME"
+ln -s "$HOME"/.dotfiles/.zsh_prompt "$HOME"
+ln -s "$HOME"/.dotfiles/.gitconfig "$HOME"
+ln -s "$HOME"/.dotfiles/.aliasrc "$HOME"
+ln -s "$HOME"/.dotfiles/.tmux.conf "$HOME"
+[ -f ~/.vimrc ] || ln -s "$HOME"/.dotfiles/.vimrc "$HOME"
 
-[ -d ~/.local ] || mkdir ~/.local && [ -d ~/.local/bin ] || mkdir ~/.local/bin
+[ -d ~/.local ] || mkdir ~/.local
+[ -d ~/.local/bin ] || mkdir ~/.local/bin
+
 for x in ~/.dotfiles/handy-scripts/*; do
-  ln -s $x ~/.local/bin/
+  ln -s "$x" ~/.local/bin/
 done
 
-[ -f ~/.local/bin/slnk ] || ln -s $HOME/.dotfiles/symlink.sh ~/.local/bin/slnk
-[ -f ~/.local/bin/ulnk ] ||  ln -s $HOME/.dotfiles/unlink.sh ~/.local/bin/ulnk
+[ -f ~/.local/bin/slnk ] || ln -s "$HOME"/.dotfiles/symlink.sh ~/.local/bin/slnk
+[ -f ~/.local/bin/ulnk ] ||  ln -s "$HOME"/.dotfiles/unlink.sh ~/.local/bin/ulnk
 
 [ -d ~/.config ] || mkdir ~/.config
 for dir in ~/.dotfiles/.config/*; do
-  config=$(basename $dir)
-  [ -d ~/.config/$config ] && rm -rdf ~/.config/$config
-  ln -s ~/.dotfiles/.config/$config ~/.config/$config
+  config=$(basename "$dir")
+  [ -d ~/.config/"$config" ] && rm -rdf ~/.config/"$config"
+  ln -s ~/.dotfiles/.config/"$config" ~/.config/"$config"
 done
