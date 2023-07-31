41f0d651df0db3a32d10d55f5fe2ead3a3cf063a
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-31 22:42:10 +0800

    Update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -6,17 +6,6 @@
 [ -f ~/.local/bin/slnk ] && unlink ~/.local/bin/slnk
 [ -f ~/.local/bin/nvim ] && unlink ~/.local/bin/nvim
 
-# GETTING BACK TO NORMAL
-mv ~/.vimrc.bak ~/.vimrc
-mv ~/.zshrc.bak ~/.zshrc
-mv ~/.bashrc.bak ~/.bashrc
-mv ~/.inputrc.bak ~/.inputrc
-mv ~/.gitconfig.bak ~/.gitconfig
-mv ~/.config/i3.bak ~/.config/i3
-mv ~/.config/nvim.bak ~/.config/nvim
-mv ~/.config/kitty.bak ~/.config/kitty
-[ -d ~/.dotfiles ] && rm -rdf ~/.dotfiles
-
 # REMOVE ALL DEPENDENCIES
 [ -d ~/.exa ] && unlink ~/.local/bin/exa && rm -rdf ~/.exa
 [ -d ~/.httpie ] && unlink ~/.local/bin/http && rm -rdf ~/.httpie
@@ -32,3 +21,6 @@ mv ~/.config/kitty.bak ~/.config/kitty
 [ -d ~/.rustup ] && rm -rdf ~/.rustup
 [ -d ~/.miniconda ] && rm -rdf ~/.miniconda
 [ -d ~/.conda ] && rm -rdf ~/.conda
+
+# REMOVE MY DOTFILES
+[ -d ~/.dotfiles ] && rm -rdf ~/.dotfiles
