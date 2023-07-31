cf3086258e1db1e01ec7dfad700425d741afe826
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-31 20:40:13 +0800

    Update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+# UNLINK ALL SYMLINKS
 [ -f ~/.local/bin/ulnk ] && ulnk
 [ -f ~/.local/bin/ulnk ] && unlink ~/.local/bin/ulnk
 [ -f ~/.local/bin/slnk ] && unlink ~/.local/bin/slnk
 [ -f ~/.local/bin/nvim ] && unlink ~/.local/bin/nvim
+
+# GETTING BACK TO NORMAL
+mv ~/.vimrc.bak ~/.vimrc
+mv ~/.zshrc.bak ~/.zshrc
+mv ~/.bashrc.bak ~/.bashrc
+mv ~/.inputrc.bak ~/.inputrc
+mv ~/.gitconfig.bak ~/.gitconfig
+mv ~/.config/i3.bak ~/.config/i3
+mv ~/.config/nvim.bak ~/.config/nvim
+mv ~/.config/kitty.bak ~/.config/kitty
+[ -d ~/.dotfiles ] && rm -rdf ~/.dotfiles
+
+# REMOVE ALL DEPENDENCIES
 [ -d ~/.exa ] && unlink ~/.local/bin/exa && rm -rdf ~/.exa
 [ -d ~/.httpie ] && unlink ~/.local/bin/http && rm -rdf ~/.httpie
 [ -d ~/.neovim ] && rm -rdf ~/.neovim
@@ -18,4 +32,3 @@
 [ -d ~/.rustup ] && rm -rdf ~/.rustup
 [ -d ~/.miniconda ] && rm -rdf ~/.miniconda
 [ -d ~/.conda ] && rm -rdf ~/.conda
-[ -d ~/.dotfiles ] && rm -rdf ~/.dotfiles
