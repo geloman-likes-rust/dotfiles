d196280767dd8425b31584bba421c180e3f93a1c
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-31 23:05:07 +0800

    Update symlink script

--- symlink.sh
+++ symlink.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 
 # ZSH CONFIGS
-ln -sf ~/.dotfiles/.zshrc "$HOME"
-ln -sf "$HOME"/.dotfiles/.zsh_prompt "$HOME"
+[ -L ~/.zshrc ] || ln -s ~/.dotfiles/.zshrc "$HOME"
+[ -L ~/.zsh_prompt ] || ln -s "$HOME"/.dotfiles/.zsh_prompt "$HOME"
 
 # BASH CONFIGS
-ln -sf ~/.dotfiles/.bashrc "$HOME"
-ln -sf ~/.dotfiles/.inputrc "$HOME"
-ln -sf "$HOME"/.dotfiles/.bash_prompt "$HOME"
+[ -L ~/.bashrc ] || ln -s ~/.dotfiles/.bashrc "$HOME"
+[ -L ~/.inputrc ] || ln -s ~/.dotfiles/.inputrc "$HOME"
+[ -L ~/.bash_prompt ] || ln -s "$HOME"/.dotfiles/.bash_prompt "$HOME"
 
 # ALIASES
-ln -sf "$HOME"/.dotfiles/.aliasrc "$HOME"
+[ -L ~/.aliasrc ] || ln -s "$HOME"/.dotfiles/.aliasrc "$HOME"
 
 # GIT CONFIGS
-ln -sf "$HOME"/.dotfiles/.gitconfig "$HOME"
+[ -L ~/.gitconfig ] || ln -s "$HOME"/.dotfiles/.gitconfig "$HOME"
 
 # TMUX CONFIGS
-ln -sf "$HOME"/.dotfiles/.tmux.conf "$HOME"
+[ -L ~/.tmux.conf ] || ln -s "$HOME"/.dotfiles/.tmux.conf "$HOME"
 
 # VIM CONFIGS
-ln -sf "$HOME"/.dotfiles/.vimrc "$HOME"
+[ -L ~/.vimrc ] || ln -s "$HOME"/.dotfiles/.vimrc "$HOME"
 
 # I3 CONFIGS
-ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
+[ -L ~/.config/i3 ] || ln -s ~/.dotfiles/.config/i3 ~/.config/i3
 
 # NVIM CONFIGS
-ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
+[ -L ~/.config/nvim ] || ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
 # KITTY CONFIGS
-ln -sf ~/.dotfiles/.config/kitty ~/.config/kitty
+[ -d ~/.config/kitty ] && mv ~/.config/kitty ~/.config/kitty.bak
+[ -L ~/.config/kitty ] || ln -s ~/.dotfiles/.config/kitty ~/.config/kitty
 
 # SHORTHAND FOR SYMLINK AND UNLINK
-[ -f ~/.local/bin/slnk ] || ln -sf ~/.dotfiles/symlink.sh ~/.local/bin/slnk
-[ -f ~/.local/bin/ulnk ] ||  ln -sf ~/.dotfiles/unlink.sh ~/.local/bin/ulnk
+[ -L ~/.local/bin/slnk ] || ln -s ~/.dotfiles/symlink.sh ~/.local/bin/slnk
+[ -L ~/.local/bin/ulnk ] ||  ln -s ~/.dotfiles/unlink.sh ~/.local/bin/ulnk
