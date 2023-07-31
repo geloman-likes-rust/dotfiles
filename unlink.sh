2fd61c657ff5753b5090d453d3616e255ae0f1d8
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-31 19:46:38 +0800

    Update unlink script

--- unlink.sh
+++ unlink.sh
@@ -7,11 +7,6 @@ unlink ~/.vimrc
 unlink ~/.bash_prompt
 unlink ~/.zsh_prompt
 
-for script in ~/.dotfiles/handy-scripts/*; do
-  file=$(basename "$script")
-  unlink ~/.local/bin/"$file"
-done
-
 for dir in ~/.dotfiles/.config/*; do
   config=$(basename "$dir")
   unlink ~/.config/"$config"
