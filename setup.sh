a321a2933e5047d80df0c4cb6b25b5baf1b7f65d
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-31 23:04:11 +0800

    Update setup config

--- setup.sh
+++ setup.sh
@@ -7,16 +7,19 @@
 [ -d ~/.config ] || mkdir ~/.config
 
 # CREATING BACKUPS
-[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
-[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
-[ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
-[ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
-[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.bak
-[ -d ~/.config/i3 ] && mv ~/.config/i3 ~/.config/i3.bak
-[ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim.bak
-[ -d ~/.config/kitty ] && mv ~/.config/kitty ~/.config/kitty.bak
+[ -f ~/.vimrc ] && mv -f ~/.vimrc ~/.vimrc.bak
+[ -f ~/.zshrc ] && mv -f ~/.zshrc ~/.zshrc.bak
+[ -f ~/.bashrc ] && mv -f ~/.bashrc ~/.bashrc.bak
+[ -f ~/.inputrc ] && mv -f ~/.inputrc ~/.inputrc.bak
+[ -f ~/.gitconfig ] && mv -f ~/.gitconfig ~/.gitconfig.bak
+[ -d ~/.config/i3 ] && mv -f ~/.config/i3 ~/.config/i3.bak
+[ -d ~/.config/nvim ] && mv -f ~/.config/nvim ~/.config/nvim.bak
+[ -d ~/.config/kitty ] && mv -f ~/.config/kitty ~/.config/kitty.bak
+#
+# cat ~/.dotfiles/.zshrc > ~/.zshrc
+# cat ~/.dotfiles/.bashrc > ~/.bashrc
 
 [ -f ~/.profile ] && echo ". \"\$HOME/.dotfiles/.default_background\"" >> ~/.profile
 
+~/.dotfiles/symlink.sh
 ~/.dotfiles/install.sh
-[ -f ~/.local/bin/slnk ] || ~/.dotfiles/symlink.sh
