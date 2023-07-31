15df9103d491f67b5db5427d00e510d53b07c91d
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-31 20:42:00 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -6,9 +6,15 @@
 [ -d ~/.local/bin ] || mkdir ~/.local/bin
 [ -d ~/.config ] || mkdir ~/.config
 
-cat ~/.dotfiles/.inputrc > ~/.inputrc
-cat ~/.dotfiles/.bashrc > ~/.bashrc
-cat ~/.dotfiles/.zshrc > ~/.zshrc
+# CREATING BACKUPS
+[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
+[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
+[ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
+[ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
+[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.bak
+[ -d ~/.config/i3 ] && mv ~/.config/i3 ~/.config/i3.bak
+[ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim.bak
+[ -d ~/.config/kitty ] && mv ~/.config/kitty ~/.config/kitty.bak
 
 [ -f ~/.profile ] && echo ". \"\$HOME/.dotfiles/.default_background\"" >> ~/.profile
 
