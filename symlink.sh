915d5e3c849a136137528c57c7727baa5e36cc4d
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-02 03:17:47 +0800

    Update symlink script

--- symlink.sh
+++ symlink.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 
 # ZSH CONFIGS
-[ -L ~/.zshrc ] || ln -s ~/.dotfiles/.zshrc "$HOME"
-[ -L ~/.zsh_prompt ] || ln -s "$HOME"/.dotfiles/.zsh_prompt "$HOME"
+[ -L ~/.zshrc ] || ln -s ~/.dotfiles/zsh/.zshrc "$HOME"
+[ -L ~/.zsh_prompt ] || ln -s "$HOME"/.dotfiles/zsh/.zsh_prompt "$HOME"
 
 # BASH CONFIGS
-[ -L ~/.bashrc ] || ln -s ~/.dotfiles/.bashrc "$HOME"
-[ -L ~/.inputrc ] || ln -s ~/.dotfiles/.inputrc "$HOME"
-[ -L ~/.bash_prompt ] || ln -s "$HOME"/.dotfiles/.bash_prompt "$HOME"
+[ -L ~/.bashrc ] || ln -s ~/.dotfiles/bash/.bashrc "$HOME"
+[ -L ~/.inputrc ] || ln -s ~/.dotfiles/bash/.inputrc "$HOME"
+[ -L ~/.bash_prompt ] || ln -s ~/.dotfiles/bash/.bash_prompt "$HOME"
 
 # ALIASES
-[ -L ~/.aliasrc ] || ln -s "$HOME"/.dotfiles/.aliasrc "$HOME"
+[ -L ~/.aliasrc ] || ln -s ~/.dotfiles/.aliasrc "$HOME"
 
 # GIT CONFIGS
-[ -L ~/.gitconfig ] || ln -s "$HOME"/.dotfiles/.gitconfig "$HOME"
+[ -L ~/.gitconfig ] || ln -s ~/.dotfiles/.gitconfig "$HOME"
 
 # TMUX CONFIGS
-[ -L ~/.tmux.conf ] || ln -s "$HOME"/.dotfiles/.tmux.conf "$HOME"
+[ -L ~/.tmux.conf ] || ln -s ~/.dotfiles/tmux/.tmux.conf "$HOME"
 
 # VIM CONFIGS
-[ -L ~/.vimrc ] || ln -s "$HOME"/.dotfiles/.vimrc "$HOME"
+[ -L ~/.vimrc ] || ln -s ~/.dotfiles/vim/.vimrc "$HOME"
 
 # I3 CONFIGS
-[ -L ~/.config/i3 ] || ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+[ -L ~/.config/i3 ] || ln -s ~/.dotfiles/i3 ~/.config/
 
 # NVIM CONFIGS
-[ -L ~/.config/nvim ] || ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+[ -L ~/.config/nvim ] || ln -s ~/.dotfiles/nvim ~/.config/
 
 # KITTY CONFIGS
-[ -L ~/.config/kitty ] || ln -s ~/.dotfiles/.config/kitty ~/.config/kitty
+[ -L ~/.config/kitty ] || ln -s ~/.dotfiles/kitty ~/.config/
 
 # SHORTHAND FOR SYMLINK AND UNLINK
 [ -L ~/.local/bin/slnk ] || ln -s ~/.dotfiles/symlink.sh ~/.local/bin/slnk
