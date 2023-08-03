61f7e5f04f8f6cb0db6bef0b9abc11a3f1b11937
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-04 03:05:09 +0800

    cursorline underlined

--- nvim/after/plugin/highlights.lua
+++ nvim/after/plugin/highlights.lua
@@ -2,3 +2,5 @@
 vim.cmd("highlight! Search guibg=#d6ffd6")
 vim.cmd("highlight! CurSearch guifg=#d6ffd6 guibg=#03c988")
 vim.cmd("highlight! IncSearch guifg=#d6ffd6 guibg=#03c988")
+vim.cmd("highlight! CursorLine gui=underline term=underline cterm=underline guibg=#21283b")
+vim.cmd("highlight! CursorLineNR term=underline cterm=underline gui=underline guibg=#21283b")
