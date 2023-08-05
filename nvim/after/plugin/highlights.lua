38701d1b8ef2ca8aaaba44c526be92a3bad2e0ab
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-05 12:25:23 +0800

    Update highlights

--- nvim/after/plugin/highlights.lua
+++ nvim/after/plugin/highlights.lua
@@ -1,6 +1,9 @@
 -- custom highlights
+vim.cmd("highlight! Visual gui=reverse")
 vim.cmd("highlight! Search guibg=#d6ffd6")
 vim.cmd("highlight! CurSearch guifg=#d6ffd6 guibg=#03c988")
 vim.cmd("highlight! IncSearch guifg=#d6ffd6 guibg=#03c988")
-vim.cmd("highlight! CursorLine gui=underline term=underline cterm=underline guibg=#21283b")
-vim.cmd("highlight! CursorLineNR term=underline cterm=underline gui=underline guibg=#21283b")
+vim.cmd("highlight! CursorLine gui=reverse guibg=#2a324a")
+vim.cmd("highlight! CursorLineNR gui=bold guibg=#2a324a")
+vim.api.nvim_command('autocmd InsertEnter * highlight! CursorLine gui=bold guibg=#2a324a')
+vim.api.nvim_command('autocmd InsertLeave * highlight! CursorLine gui=reverse guibg=#2a324a')
