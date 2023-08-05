2d59567d9af787d3c1da4cee3ae5bce5efc05340
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-05 19:33:38 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,10 @@
 # CREATING BACKUPS
 [ -f ~/.vimrc ] && mv -f ~/.vimrc ~/.vimrc.bak
 [ -f ~/.zshrc ] && mv -f ~/.zshrc ~/.zshrc.bak
+[ -f ~/.zshenv ] && mv -f ~/.zshenv ~/.zshenv.bak
+[ -f ~/.zprofile ] && mv -f ~/.zprofile ~/.zprofile.bak
 [ -f ~/.bashrc ] && mv -f ~/.bashrc ~/.bashrc.bak
+[ -f ~/.bash_profile ] && mv -f ~/.bash_profile ~/.bash_profile.bak
 [ -f ~/.inputrc ] && mv -f ~/.inputrc ~/.inputrc.bak
 [ -f ~/.gitconfig ] && mv -f ~/.gitconfig ~/.gitconfig.bak
 [ -d ~/.config/i3 ] && mv -f ~/.config/i3 ~/.config/i3.bak
