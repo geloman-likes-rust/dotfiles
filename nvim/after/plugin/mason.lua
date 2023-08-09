d3f628b7cda7de070436572e76e641a01d715b2a
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-09 15:33:59 +0800

    Rename nmap as keymap

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,18 +1,18 @@
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
+  local keymap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
     end
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  keymap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  keymap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  keymap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  keymap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  keymap('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
+  keymap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  keymap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
