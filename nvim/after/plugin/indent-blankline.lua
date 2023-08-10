7c7bd391e8633dce7e5532948b62dd1d7172ed30
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-10 16:07:42 +0800

    Update indent-blankline config

---
+++ nvim/after/plugin/indent-blankline.lua
@@ -0,0 +1,5 @@
+vim.g.indent_blankline_context_char = '┃'
+require("indent_blankline").setup {
+  show_current_context = true,
+  show_current_context_start = true,
+}
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,1 @@
-return {
-  'lukas-reineke/indent-blankline.nvim',
-  opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-  },
-}
+return { 'lukas-reineke/indent-blankline.nvim' }
