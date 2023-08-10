326b055c1cc3c29f4b2eb2d7b71476d3b93e146c
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-10 16:19:39 +0800

    dont show current context start

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -1,5 +1,4 @@
-vim.g.indent_blankline_context_char = '┃'
+vim.g.indent_blankline_context_char = '┊'
 require("indent_blankline").setup {
   show_current_context = true,
-  show_current_context_start = true,
 }
