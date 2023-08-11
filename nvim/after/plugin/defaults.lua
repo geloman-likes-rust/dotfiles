9089dc72a300f9560e21634ad3c843a98e78820e
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-11 15:51:24 +0800

    set shiftwidth to 4

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -2,9 +2,9 @@
 vim.o.showmode = false
 
 -- tab space
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.autoindent = true
