15aa4ed25ab7bdc845131247828af346fcc116d6
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-14 00:58:52 +0800

    Update treesitter config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,17 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  priority = 1500,
-  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-  event = "BufReadPost",
-  config = function()
-    local parsers = { 'lua', 'rust', 'html', 'tsx', 'typescript', 'javascript', 'vimdoc' }
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = parsers,
-      auto_install = false,
-      highlight = { enable = true },
-      indent = { enable = true, disable = { 'python', 'html' } },
-    }
-  end,
-  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-  build = ":TSUpdate",
+    'nvim-treesitter/nvim-treesitter',
+    priority = 1500,
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    event = "BufReadPost",
+    config = function()
+        local parsers = { 'lua', 'rust', 'html', 'tsx', 'typescript', 'javascript', 'vimdoc' }
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = parsers,
+            auto_install = false,
+            highlight = { enable = true },
+            indent = { enable = true, disable = { 'python', 'html' } },
+        }
+    end,
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', 'nvim-treesitter/playground' },
+    build = ":TSUpdate",
 }
