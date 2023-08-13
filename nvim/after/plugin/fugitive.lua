900a5951196d75f0f3b355fe1d1e7c7d284bd5e3
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-13 18:43:11 +0800

    lazy load fugitive

--- nvim/after/plugin/fugitive.lua
+++
@@ -1,6 +0,0 @@
--- custom keymaps
-vim.keymap.set("n", "<leader>gc", ":Git commit<cr>", { noremap = true, silent = true, desc = "[G]it [C]ommit" })
-vim.keymap.set("n", "<leader>gp", ":Git push<cr>", { noremap = true, desc = "[G]it [P]ush" })
-vim.keymap.set("n", "<leader>G", ":vertical Git<cr>", { noremap = true, silent = true, desc = "[G]it fugitive" })
-vim.keymap.set("n", "<leader>gds", ":Gvdiffsplit<cr>",
-  { noremap = true, silent = true, desc = "[G]it [D]iff [S]plit" })
