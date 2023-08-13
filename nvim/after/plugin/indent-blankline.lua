c2a5c24c2f856652ac390d96703c3d8b825968cb
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-13 15:40:52 +0800

    Lazy load indent-blankline

--- nvim/after/plugin/indent-blankline.lua
+++
@@ -1,4 +0,0 @@
-vim.g.indent_blankline_context_char = '┊'
-require("indent_blankline").setup {
-  show_current_context = true,
-}
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,1 +1,10 @@
-return { 'lukas-reineke/indent-blankline.nvim' }
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  event = "CursorMoved",
+  config = function()
+    vim.g.indent_blankline_context_char = '┊'
+    require("indent_blankline").setup {
+      show_current_context = true
+    }
+  end,
+}
