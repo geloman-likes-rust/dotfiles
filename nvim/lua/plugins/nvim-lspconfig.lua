1b7513d420c43ac820af2625c4a430a1171bfcbd
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-18 20:55:46 +0800

    Lazy load mason

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,4 @@
--- LSP Configuration & Plugins
 return {
   'neovim/nvim-lspconfig',
-  priority = 1000,
-  dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true },
-    'williamboman/mason-lspconfig.nvim',
-
-  },
+  dependencies = { 'williamboman/mason-lspconfig.nvim' },
 }
