280e7030b96bdbca3e59c35d67c5d362dc64c96c
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-18 20:10:02 +0800

    Add friendly-snippets

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,5 +1,10 @@
 return {
   -- Autocompletion
   'hrsh7th/nvim-cmp',
-  dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  dependencies = { 'hrsh7th/cmp-nvim-lsp',
+    {
+      'L3MON4D3/LuaSnip',
+      config = function() require("luasnip.loaders.from_vscode").lazy_load() end,
+      dependencies = { "rafamadriz/friendly-snippets" }
+    }, 'saadparwaiz1/cmp_luasnip' },
 }
