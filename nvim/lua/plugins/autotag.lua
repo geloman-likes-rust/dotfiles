c1b56adf278771d0d8bbfbada12f81cc7a02e2ef
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-19 18:58:47 +0800

    properly lazy load ts-autotag

--- nvim/lua/plugins/autotag.lua
+++ nvim/lua/plugins/autotag.lua
@@ -1,1 +1,6 @@
-return { "windwp/nvim-ts-autotag", event = "InsertEnter", opts = { autotag = { enable = true } } }
+return {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    opts = { autotag = { enable = true } },
+    config = function(_, opts) require('nvim-ts-autotag').setup(opts) end
+}
