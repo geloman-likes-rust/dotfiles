0f6cd06e8d78d6e2a7ac681487cc6c69ea20d5eb
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-19 19:12:46 +0800

    i dont like format on save

--- nvim/lua/config/plugin/nvim-lspconfig.lua
+++ nvim/lua/config/plugin/nvim-lspconfig.lua
@@ -20,8 +20,6 @@ local on_attach = function(_, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
-    -- format on save
-    vim.api.nvim_command('autocmd BufWritePre * Format')
 end
 
 -- Enable the following language servers
