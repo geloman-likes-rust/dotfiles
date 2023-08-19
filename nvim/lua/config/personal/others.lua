d5f861d76f8aba5685d101dc033504e8f198a9ac
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-19 11:24:13 +0800

    Add neovide configs

--- nvim/lua/config/personal/others.lua
+++ nvim/lua/config/personal/others.lua
@@ -4,3 +4,17 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
+
+-- Neovide configs
+if vim.g.neovide then
+    vim.o.guifont = "Hack Nerd Font:h16" -- text below applies for VimScript
+    vim.g.neovide_transparency = 0.8
+    vim.g.transparency = 0.8
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+    vim.g.neovide_background_color = "#1A212E"
+    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_confirm_quit = false
+end
