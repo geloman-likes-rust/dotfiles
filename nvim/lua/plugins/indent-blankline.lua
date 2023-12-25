7229ebaa102fc389b0372531e063525bc7c74b20
geloman <geloman-likes-rust@users.noreply.github.com>
2023-12-25 18:07:51 +0800

    Update indent-blankline config

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,12 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
-  event = "CursorMoved",
-  config = function()
-    vim.g.indent_blankline_context_char = '┊'
-    require("indent_blankline").setup {
-      show_current_context = true
-    }
-  end,
+  -- event = "CursorMoved",
+    main = "ibl",
+    opts = {},
+  -- config = function()
+  --   vim.g.indent_blankline_context_char = '┊'
+  --   require("indent_blankline").setup {
+  --     show_current_context = true
+  --   }
+  -- end,
 }
