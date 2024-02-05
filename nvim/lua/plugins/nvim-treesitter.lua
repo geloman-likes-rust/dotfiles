36c272c1014d28170a0deb7804fdce51c0fb2638
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-05 22:34:55 +0800

    Update treesitter config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,13 +4,18 @@ return {
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     event = "BufReadPost",
     config = function()
-        local parsers = { 'lua', 'rust', 'ocaml', 'html', 'tsx', 'typescript', 'javascript', 'vimdoc' }
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = parsers,
-            auto_install = false,
-            highlight = { enable = true },
-            indent = { enable = true, disable = { 'python', 'html' } },
-        }
+        vim.defer_fn(function()
+            local parsers = { 'lua', 'rust', 'dockerfile', 'html', 'tsx', 'typescript', 'javascript', 'vimdoc' }
+            require('nvim-treesitter.configs').setup {
+                modules = {},
+                auto_install = false,
+                sync_install = false,
+                ignore_install = {},
+                ensure_installed = parsers,
+                highlight = { enable = true },
+                indent = { enable = true, disable = { 'python', 'html' } },
+            }
+        end, 0)
     end,
     dependencies = { 'nvim-treesitter/playground' },
     build = ":TSUpdate",
