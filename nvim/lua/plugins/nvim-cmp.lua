ddc4bb8494347f663411abbafffe408bdd7ec442
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-05 22:48:43 +0800

    Add plugin: hrsh7th/cmp-path

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -7,6 +7,7 @@ return {
     end,
     dependencies = {
         'L3MON4D3/LuaSnip',
+        'hrsh7th/cmp-path',
         'hrsh7th/cmp-nvim-lsp',
         'saadparwaiz1/cmp_luasnip',
         "rafamadriz/friendly-snippets",
