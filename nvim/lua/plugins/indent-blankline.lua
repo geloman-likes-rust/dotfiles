229dcd749c08a987cf51c71821416f016f516b94
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-06 00:16:29 +0800

    Update indent-blankline configs

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,12 +1,31 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  -- event = "CursorMoved",
+    'lukas-reineke/indent-blankline.nvim',
+    event = "CursorMoved",
     main = "ibl",
-    opts = {},
-  -- config = function()
-  --   vim.g.indent_blankline_context_char = '┊'
-  --   require("indent_blankline").setup {
-  --     show_current_context = true
-  --   }
-  -- end,
+    config = function()
+        local highlight = {
+            "RainbowRed",
+            "RainbowYellow",
+            "RainbowBlue",
+            "RainbowOrange",
+            "RainbowGreen",
+            "RainbowViolet",
+            "RainbowCyan",
+        }
+
+        local hooks = require "ibl.hooks"
+        -- create the highlight groups in the highlight setup hook, so they are reset
+        -- every time the colorscheme changes
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+            vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+            vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+            vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+            vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+            vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+            vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        end)
+
+        require("ibl").setup { indent = { char = '┊', highlight = highlight } }
+    end,
 }
