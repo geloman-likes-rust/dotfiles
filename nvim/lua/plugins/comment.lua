3d3c623626bf6ea539f0433410f021cde46dc92d
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-06 00:49:12 +0800

    Load comment plugin on InsertEnter

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,6 +1,7 @@
 return {
     "numToStr/Comment.nvim",
     keys = { "gc", "gb", "gcc", "gbc" },
+    event = { "InsertEnter" },
     config = function()
         require("Comment").setup()
     end,
