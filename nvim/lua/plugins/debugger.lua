ef49c9e9b7317fcd624e49a523bf08ded093ef0e
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-11 06:38:15 +0800

    Update nvim-dap

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -1,5 +1,6 @@
 return {
     'mfussenegger/nvim-dap',
     cmd = { "DapContinue", "DapShowLog", "DapToggleRepl", "DapToggleBreakpoint", "DapLoadLaunchJSON", "DapRestartFrame",
-        "DapSetLogLevel", "DapStepInto", "DapStepOut", "DapStepOver", "DapTerminate" }
+        "DapSetLogLevel", "DapStepInto", "DapStepOut", "DapStepOver", "DapTerminate" },
+    dependencies = { "rcarriga/nvim-dap-ui" }
 }
