7f93fa10b46a1f0fc5b050e10af805915dedc94a
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-19 04:18:17 +0800

    Add plugin: treesitter-context

---
+++ nvim/lua/plugins/treesitter-context.lua
@@ -0,0 +1,14 @@
+return {
+    'nvim-treesitter/nvim-treesitter-context',
+    cmd = { 'TSContextEnable', 'TSContextDisable', 'TSContextToggle' },
+    keys = {
+        { '<leader>te', desc = 'Enable TSContext' },
+        { '<leader>td', desc = 'Disable TSContext' },
+        { '<leader>tt', desc = 'Toggle TSContext' },
+    },
+    config = function()
+        vim.keymap.set('n', '<leader>te', function() vim.cmd('TSContextEnable') end, {})
+        vim.keymap.set('n', '<leader>td', function() vim.cmd('TSContextDisable') end, {})
+        vim.keymap.set('n', '<leader>tt', function() vim.cmd('TSContextToggle') end, {})
+    end,
+}
