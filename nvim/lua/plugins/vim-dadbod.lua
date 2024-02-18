688816f21815e0fbd7b590e7d380943bf445ac79
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-19 01:31:12 +0800

    Set keymap for toggling vim-dadbod-ui

--- nvim/lua/plugins/vim-dadbod.lua
+++ nvim/lua/plugins/vim-dadbod.lua
@@ -1,5 +1,8 @@
 return {
     "tpope/vim-dadbod",
-    cmd = { "DB", "DBUI" },
+    cmd = { "DB", "DBUI", "DBUIToggle" },
+    keys = function()
+        return { { "<leader>db", function() vim.cmd("DBUIToggle") end, desc = "DBUIToggle" } }
+    end,
     dependencies = { "kristijanhusak/vim-dadbod-ui", "kristijanhusak/vim-dadbod-completion" },
 }
