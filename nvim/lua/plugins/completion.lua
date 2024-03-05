635884a3d311ef55e2c8a4943ca7ecc38e112dc4
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-05 17:57:09 +0800

    Exclude snippets for rust

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -12,7 +12,7 @@ return {
     config = function()
         local cmp = require 'cmp'
         local luasnip = require 'luasnip'
-        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load { exclude = { 'rust' } }
         luasnip.config.setup {}
 
         cmp.setup {
