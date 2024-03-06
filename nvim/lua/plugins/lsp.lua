b91563088d308c340356fa86d40f2b8bb6222e9a
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-06 23:27:14 +0800

    Update lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,76 +1,39 @@
-local on_attach = function(_, bufnr)
-    local keymap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    -- LSP SPECIFIC KEYMAPS
-    keymap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    keymap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-    keymap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    keymap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    keymap('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
-    keymap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    keymap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-    keymap('<leader>do', vim.diagnostic.open_float, 'Open Floating Diagnostic')
-    keymap('<leader>dn', vim.diagnostic.goto_next, 'Next Diagnostic')
-    keymap('<leader>dp', vim.diagnostic.goto_prev, 'Previous Diagnostic')
-    keymap('<leader>fm', vim.lsp.buf.format, 'Format Current Buffer')
-
-    -- format on save
-    vim.api.nvim_create_autocmd("BufWritePost",
-        { callback = function() vim.lsp.buf.format() end })
-end
-
 return {
-    {
-        "williamboman/mason.nvim",
-        cmd = { "Mason", "MasonLog", "MasonUpdate", "MasonInstall", "MasonUninstall", "MasonUninstallAll" },
-        opts = { ui = { border = "rounded" } }
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        config = function()
-            -- Default DiagnosticSign
-            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl })
-            end
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    config = function()
+        require('neodev').setup()
+        local lspconfig = require("lspconfig")
+        local util = lspconfig.util
 
-            local mason_lspconfig = require("mason-lspconfig")
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        lspconfig.lua_ls.setup {
+            autostart = true,
+            cmd = { 'lua-language-server' },
+            capabilities = capabilities,
+        }
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        -- lspconfig.rust_analyzer.setup {
+        --     autostart = true,
+        --     cmd = { 'rust-analyzer' },
+        --     capabilities = capabilities,
+        --     root_dir = util.root_pattern('Cargo.toml', 'Cargo.lock')
+        -- }
 
-            local language_servers = {
-                lua_ls = {
-                    Lua = { diagnostics = { globals = { "vim" } } }
-                },
-            }
-            mason_lspconfig.setup { ensure_installed = vim.tbl_keys(language_servers) }
+        -- lspconfig.omnisharp.setup {
+        --     cmd = { "dotnet", "$HOME/.omnisharp-roslyn/OmniSharp.dll" },
+        --     enable_editorconfig_support = true,
+        --     enable_roslyn_analyzers = true,
+        --     analyze_open_documents_only = true,
+        -- }
 
-            mason_lspconfig.setup_handlers {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                        settings = language_servers[server_name],
-                        filetypes = (language_servers[server_name] or {}).filetypes,
-                    }
-                end,
-                ["lua_ls"] = function()
-                    require("neodev").setup()
-                    require("lspconfig").lua_ls.setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-            }
-        end,
-        dependencies = { "neovim/nvim-lspconfig" },
-    },
+        -- lspconfig.lua_ls.setup {
+        --     autostart = true,
+        --     cmd = { 'typescript-language-server' },
+        --     capabilities = capabilities,
+        --     root_dir = util.root_pattern('package.json', 'package-lock.json')
+        -- }
+    end,
+    dependencies = { "folke/neodev.nvim", cmd = "Neoconf" },
 }
