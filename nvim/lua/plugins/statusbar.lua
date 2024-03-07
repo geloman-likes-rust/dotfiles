d5e3ab46b402bbd7a4be8fc52d5a27692c164b84
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-08 06:33:52 +0800

    Update statusline

--- nvim/lua/plugins/statusbar.lua
+++ nvim/lua/plugins/statusbar.lua
@@ -11,14 +11,15 @@ return {
             component_separators = { left = '│', right = '│' },
             -- component_separators = { left = '┊', right = '' },
             -- section_separators = { left = '', right = '' },
+            disabled_filetypes = { 'NvimTree' },
         },
         sections = {
-            lualine_a = { 'mode' },
+            lualine_a = { { 'mode', separator = { left = '', right = '' } } },
             lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename' },
+            lualine_c = { '%=', { 'filename', color = 'lualine_b_normal', separator = { left = '', right = '' } } },
             lualine_x = { 'filetype' },
             lualine_y = { 'progress' },
-            lualine_z = { 'location' }
+            lualine_z = { { 'location', separator = { left = '', right = '' } } }
         },
     }
 }
