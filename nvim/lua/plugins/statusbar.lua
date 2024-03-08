e713c9a80f469172b4cb49380b9858f7e5306b06
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-08 16:53:07 +0800

    Update statusbar

--- nvim/lua/plugins/statusbar.lua
+++ nvim/lua/plugins/statusbar.lua
@@ -21,5 +21,13 @@ return {
             lualine_y = { 'progress' },
             lualine_z = { { 'location', separator = { left = '', right = '' } } }
         },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { '%=', { 'branch', color = 'lualine_b_normal', separator = { left = '', right = '' } }, { 'filename', color = 'lualine_a_normal', separator = { left = '', right = '' } } },
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {},
+        },
     }
 }
