13879a2a72336f855b162231dbe8600e9e28a274
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-09 15:25:57 +0800

    Update vim-dadbod

--- nvim/lua/plugins/vim-dadbod.lua
+++ nvim/lua/plugins/vim-dadbod.lua
@@ -1,9 +1,6 @@
 return {
     "tpope/vim-dadbod",
     cmd = { "DB", "DBUI", "DBUIToggle" },
-    keys = { { "<leader>db", desc = "DBUIToggle" } },
-    config = function()
-        vim.keymap.set('n', '<leader>db', function() vim.cmd('DBUIToggle') end, {})
-    end,
+    keys = { { "<leader>db", '<cmd>DBUIToggle<cr>', desc = "DBUIToggle" } },
     dependencies = { "kristijanhusak/vim-dadbod-ui", --[["kristijanhusak/vim-dadbod-completion"]] },
 }
