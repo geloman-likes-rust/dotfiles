e28aacc16cb70b940c821fb68a738ef86f2be703
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-09 15:23:57 +0800

    Update NvimTree

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -2,7 +2,5 @@ return {
     'nvim-tree/nvim-tree.lua',
     opts = {},
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = function()
-        return { { '<leader>e', function() vim.cmd('NvimTreeToggle') end, desc = "Toogle File Explorer" } }
-    end,
+    keys = { { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle File Explorer' } },
 }
