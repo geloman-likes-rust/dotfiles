abc282b001c36c17f053a0d766e2d90cbee7b6a9
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-10 19:35:25 +0800

    Add plugin trouble

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+    'folke/trouble.nvim',
+    cmd = { 'Trouble', 'TroubleClose', 'TroubleToggle', 'TroubleRefresh' },
+    keys = {
+        { '<leader>tw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Toggle Workspace Diagnostics' },
+        { '<leader>tl', '<cmd>TroubleToggle lsp_references<cr>',        desc = 'Toggle Lsp References' },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+}
