925cb29d0805a625ebed71a5526c546d46179f8b
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-11 02:43:11 +0800

    Update lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,12 +8,15 @@ return {
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+        --[[ LUA LSP ]]
         lspconfig.lua_ls.setup {
             autostart = true,
             cmd = { 'lua-language-server' },
             capabilities = capabilities,
         }
 
+        --[[ RUST LSP ]]
         lspconfig.rust_analyzer.setup {
             autostart = true,
             cmd = { 'rust-analyzer' },
@@ -21,6 +24,15 @@ return {
             root_dir = util.root_pattern('Cargo.toml', 'Cargo.lock', 'rust-project.json')
         }
 
+        --[[ GOLANG LSP ]]
+        lspconfig.gopls.setup {
+            autostart = true,
+            cmd = { 'gopls' },
+            capabilities = capabilities,
+            root_dir = util.root_pattern("go.work", "go.mod", ".git")
+        }
+
+        --[[ CSHARP LSP ]]
         lspconfig.omnisharp.setup {
             name = 'omnisharp-roslyn',
             autostart = true,
@@ -32,10 +44,20 @@ return {
             root_dir = util.root_pattern("*.sln", "*.csproj", "omnisharp.json", "function.json")
         }
 
+        --[[ TYPESCRIPT LSP ]]
         lspconfig.tsserver.setup {
             autostart = true,
-            cmd = { 'typescript-language-server' },
+            cmd = { 'typescript-language-server', '--stdio' },
             capabilities = capabilities,
+            root_dir = util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")
+        }
+
+        --[[ SVELTE LSP ]]
+        lspconfig.svelte.setup {
+            autostart = true,
+            cmd = { "svelteserver", "--stdio" },
+            capabilities = capabilities,
+            root_dir = util.root_pattern("package.json", ".git")
         }
     end,
     dependencies = { "folke/neodev.nvim", cmd = "Neoconf" },
