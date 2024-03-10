2a7a0709f21face0b34e93da8e952f9b8a729e5d
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-10 19:46:57 +0800

    Update all imports

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-require("config.lazy")
-require("config.autocmd")
-require("config.options")
-require("config.keymaps")
-require("config.highlights")
-require("config.neovide")
+require 'config.lazy'
+require 'config.autocmd'
+require 'config.options'
+require 'config.keymaps'
+require 'config.highlights'
+require 'config.neovide'
