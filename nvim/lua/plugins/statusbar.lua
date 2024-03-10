6d61e8d46765cd45aa98101c4ecfacdf065925ca
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-10 23:48:13 +0800

    Add onclick function

--- nvim/lua/plugins/statusbar.lua
+++ nvim/lua/plugins/statusbar.lua
@@ -16,7 +16,14 @@ return {
         sections = {
             lualine_a = { { 'mode', separator = { left = '', right = '' } } },
             lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { '%=', { 'filename', color = 'lualine_b_normal', separator = { left = '', right = '' } } },
+            lualine_c = { '%=', {
+                'filename',
+                on_click = function()
+                    print(vim.fn.expand('%:p'))
+                end,
+                color = 'lualine_b_normal',
+                separator = { left = '', right = '' }
+            } },
             lualine_x = { 'filetype' },
             lualine_y = { 'progress' },
             lualine_z = { { 'location', separator = { left = '', right = '' } } }
