b9ae0cc14bab7eb5ab51d9b5778d20710bf3fe16
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-13 00:44:47 +0800

    Remove unused plugins

--- nvim/lua/plugins/games.lua
+++
@@ -1,5 +0,0 @@
-return {
-    { 'ThePrimeagen/vim-be-good', cmd = "VimBeGood", lazy = true },
-    { 'alec-gibson/nvim-tetris',  cmd = "Tetris",    lazy = true },
-    { 'jim-fx/sudoku.nvim',       cmd = "Sudoku",    lazy = true },
-}
