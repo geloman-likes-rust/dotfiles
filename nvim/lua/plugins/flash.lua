33a0a7d8205f60c0771cd2d6505e9fc40d5c4f3c
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-12 18:56:25 +0800

    Update flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,7 +1,13 @@
 return {
     "folke/flash.nvim",
     opts = {},
-    keys = {
-        { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    },
+    keys = function()
+        local flash = require 'flash'
+        local toggle = function() flash.toggle() end
+        local jump = function() flash.jump() end
+        return {
+            { "<leader>s",  jump,   desc = "Flash Jump" },
+            { "<leader>tf", toggle, desc = "Flash Toggle" },
+        }
+    end
 }
