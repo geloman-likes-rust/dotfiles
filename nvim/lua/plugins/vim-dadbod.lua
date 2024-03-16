8748eb051ea1c1dfdbd53e4cec5102c6f7d3aea0
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-16 19:08:07 +0800

    Update vim-dadbod config

--- nvim/lua/plugins/vim-dadbod.lua
+++ nvim/lua/plugins/vim-dadbod.lua
@@ -1,6 +1,10 @@
 return {
     "tpope/vim-dadbod",
-    cmd = { "DB", "DBUI", "DBUIToggle" },
-    keys = { { "<leader>db", '<cmd>DBUIToggle<cr>', desc = "DBUIToggle" } },
+    cmd = { "DB", "DBUI", "DBUIClose", "DBUIToggle", "DBUIAddConnection" },
+    keys = {
+        { "<leader>dt", "<cmd>DBUIToggle<cr>",        desc = "DBUIToggle" },
+        { "<leader>dq", "<cmd>DBUIClose<cr>",         desc = "DBUIClose" },
+        { "<leader>da", "<cmd>DBUIAddConnection<cr>", desc = "DBUIAddConnection" },
+    },
     dependencies = { "kristijanhusak/vim-dadbod-ui", --[["kristijanhusak/vim-dadbod-completion"]] },
 }
