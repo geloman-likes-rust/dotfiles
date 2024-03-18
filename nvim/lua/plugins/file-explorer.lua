28f65a861f6709f789525e288595fc3169057203
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-18 19:47:31 +0800

    don't hide dotfiles pls

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -1,4 +1,11 @@
 return {
     'nvim-neo-tree/neo-tree.nvim',
     keys = { { '<leader>e', '<cmd>Neotree toggle float<cr>', desc = 'Toggle File Explorer' } },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+            }
+        },
+    },
 }
