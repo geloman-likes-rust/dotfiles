d4a22c84cea2314b9fced0039b0f08f021743aae
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-19 00:08:51 +0800

    set limit to sticky code block

--- nvim/lua/plugins/treesitter-context.lua
+++ nvim/lua/plugins/treesitter-context.lua
@@ -6,4 +6,7 @@ return {
         vim.keymap.set('n', '<leader>td', function() vim.cmd('TSContextDisable') end, { desc = 'Disable TSContext' })
         vim.keymap.set('n', '<leader>tt', function() vim.cmd('TSContextToggle') end, { desc = 'Toggle TSContext' })
     end,
+    opts = {
+        max_lines = 4, -- prevent from taking over my entire screen.
+    }
 }
