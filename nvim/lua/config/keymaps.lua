2ff893731b106837d7c5f00b33de12e0e2e77742
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-18 21:00:46 +0800

    Update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,38 +16,12 @@ keymap("n", "<leader>l2", "<cmd>set laststatus=2<cr>", { noremap = true, silent 
 -- toggle number line
 keymap("n", "<leader>ln", toggle_numberln, { noremap = true, silent = true, desc = "toggle number line" })
 
--- window's resize
-keymap("n", "=", "<cmd>vertical resize +5<cr>", { noremap = true, silent = true, desc = "[=] (+) vertical resize" })
-keymap("n", "-", "<cmd>vertical resize -5<cr>", { noremap = true, silent = true, desc = "[-] (-) vertical resize" })
-keymap("n", "_", "<cmd>horizontal resize -3<cr>", { noremap = true, silent = true, desc = "[_] (-) horizontal resize" })
-keymap("n", "+", "<cmd>horizontal resize +3<cr>", { noremap = true, silent = true, desc = "[+] (+) horizontal resize" })
-
 -- toggle highlight search
 keymap("n", "<leader>hl", ":set hlsearch!<cr>", { noremap = true, silent = true, desc = "toggle [H]ighlight [S]earch" })
 
--- close all buffers
-keymap("n", "<C-q>", function() vim.cmd("qall!") end)
-
--- toggle cursorline
-keymap("n", "<cr>", ":set cursorline!<cr>", { noremap = true, silent = true })
-
--- move cursor to bottom/top and center the screen
-keymap("n", "H", "Hzz")
+-- move cursor to bottom and center the screen
 keymap("n", "L", "Lzz")
 keymap("n", "G", "Gzz")
-
-keymap("n", "<leader>b", ":buffers<cr>", { silent = true })
--- switch buffers
-keymap("n", "<C-n>", ":bNext<cr>", { silent = true })
-keymap("n", "<left>", ":bprevious<cr>", { silent = true })
-keymap("n", "<right>", ":bNext<cr>", { silent = true })
-
--- switch tabs
-keymap("n", "<S-tab>", ":tabprevious<cr>", { silent = true })
-keymap("n", "<tab>", ":tabNext<cr>", { silent = true })
-
--- close tab
-keymap("n", "<leader>tq", ":tabclose<cr>", { silent = true })
 
 -- navigate across wrapped lines
 keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -55,4 +29,8 @@ keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- show file path of current buffer
-keymap('n', '<leader>pwd', function() print(vim.fn.expand("%:p")) end, { desc = 'show current buffer file path' })
+local hidden = true
+keymap('n', '<leader>pwd', function()
+    hidden = not hidden
+    if hidden then vim.o.winbar = '' else vim.o.winbar = '%= %f %=' end
+end, { desc = 'show current buffer file path' })
