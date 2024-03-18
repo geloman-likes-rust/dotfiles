632cc9c1855b55457766b45619e1a15913abfd4c
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-19 00:07:07 +0800

    Add new plugin for formatting

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,23 @@
+return {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre',
+    opts = {
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            go = { 'goimports', 'gofmt' },
+            rust = { 'rustfmt' },
+            typescript = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
+            javascript = { { "prettierd", "prettier" } },
+            javascriptreact = { { "prettierd", "prettier" } },
+            json = { { "prettierd", "prettier" } },
+            html = { { "prettierd", "prettier" } },
+            css = { { "prettierd", "prettier" } },
+        },
+        format_on_save = {
+            lsp_fallback = true,
+            timeout_ms = 500,
+        },
+        notify_on_error = false
+    }
+}
