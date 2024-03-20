a0bd77166d74acab938b3cd0342d625e37911760
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-20 21:26:46 +0800

    Update keymap

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,7 +3,7 @@ return {
     cmd = { 'Trouble', 'TroubleClose', 'TroubleToggle', 'TroubleRefresh' },
     keys = {
         { '<leader>tw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Toggle Workspace Diagnostics' },
-        { '<leader>tl', '<cmd>TroubleToggle lsp_references<cr>',        desc = 'Toggle Lsp References' },
+        { '<leader>tr', '<cmd>TroubleToggle lsp_references<cr>',        desc = 'Toggle Lsp References' },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
 }
