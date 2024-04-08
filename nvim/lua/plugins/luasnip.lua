b06eb0a74bc2ee5d1134a778bbe354998b5854b5
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-09 02:02:58 +0800

    Update luasnip config

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,6 +1,18 @@
 return {
     'L3MON4D3/LuaSnip',
-    keys = { { '<C-space>', function() require('luasnip').expand() end, mode = 'i' } },
+    keys = function()
+        local luasnip = require 'luasnip'
+        local expand = function() luasnip.expand() end
+        local change_choice = function()
+            if luasnip.choice_active() then
+                luasnip.change_choice(1)
+            end
+        end
+        return {
+            { '<C-space>', mode = 'i',          expand },
+            { '<C-e>',     mode = { 'i', 's' }, change_choice },
+        }
+    end,
     config = function()
         local luasnip = require 'luasnip'
         luasnip.add_snippets('lua', require('config.snippets.lua'))
