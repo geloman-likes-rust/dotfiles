9242cb68bd7b00645f198b78980b71836e31a285
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-09 20:42:48 +0800

    Update snippet config for lua

--- nvim/lua/config/snippets/lua.lua
+++ nvim/lua/config/snippets/lua.lua
@@ -1,22 +1,19 @@
-local luasnip = require 'luasnip'
-local rep = require('luasnip.extras').rep
-local fmt = require('luasnip.extras.fmt').fmt
-local fmta = require('luasnip.extras.fmt').fmta
+return function(luasnip, format, extras)
+    local rep = extras.rep
+    local fmt = format.fmt
+    local i = luasnip.insert_node
+    local snippet = luasnip.snippet
 
-local t = luasnip.text_node
-local i = luasnip.insert_node
-local c = luasnip.choice_node
-local snippet = luasnip.snippet
-
-return {
-    snippet('ifn', fmt(
-        [[
+    return {
+        snippet('ifn', fmt(
+            [[
             function() {} end
         ]], { i(1) }
-    )),
-    snippet('lr', fmt(
-        [[
+        )),
+        snippet('lr', fmt(
+            [[
             local {} = require('{}')
         ]], { i(1), rep(1) }
-    )),
-}
+        )),
+    }
+end
