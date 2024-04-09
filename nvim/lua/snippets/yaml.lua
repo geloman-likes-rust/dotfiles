100e4bcb1f01b6e0670bd117bd2791bb679287c3
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-10 06:40:33 +0800

    Update docker compose snippets for yaml

--- nvim/lua/snippets/yaml.lua
+++ nvim/lua/snippets/yaml.lua
@@ -23,32 +23,58 @@ return function(luasnip, format, extras)
               image: {image}
               ports:
                 - {your_port}:{container_port}
+              restart: on-failure
               environment:
-                - {environment}=
+                - {environment}=${{{}:-""}}
       ]],
       {
         container = i(1, 'container'),
         image = i(2, 'image'),
         your_port = i(3, 'port'),
         container_port = rep(3),
-        environment = i(4, 'MY_ENVIRONMENT')
+        environment = i(4, 'MY_ENVIRONMENT'),
+        rep(4)
       }
     )),
     snippet('container', fmt(
       [[
         {container}:
           image: {image}
-          port:
+          ports:
             - {your_port}:{container_port}
+          restart: on-failure
           environment:
-            - {environment}=
+            - {environment}=${{{}:-""}}
       ]], {
         container = i(1, 'container'),
         image = i(2, 'image'),
         your_port = i(3, 'port'),
         container_port = rep(3),
-        environment = i(4, 'MY_ENVIRONMENT')
+        environment = i(4, 'MY_ENVIRONMENT'),
+        rep(4)
       }
     )),
+    snippet('ENVIRONMENT', fmt(
+      [[
+        - {environment}=${{{}:-""}}
+      ]], { environment = i(1, 'MY_ENVIRONMENT'), rep(1) }
+    )),
+    snippet('depends_on', fmt(
+      [[
+        depends_on:
+          {container}:
+            condition: {condition}
+      ]], { container = i(1, 'container'), condition = i(2, 'service_healthy') }
+    )),
+    snippet('healthcheck', fmt(
+      [[
+        healthcheck:
+          test: ["CMD", "{}"]
+          interval: 5s
+          timeout: 10s
+          retries: 15
+          start_period: 10s
+      ]], { i(1) }
+    ))
   }
 end
