b420a8a8513c950944217e75aaa0f71ad5b9bb9d
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-10 00:01:56 +0800

    Don't load sql snippets from vscode snippets

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -12,7 +12,7 @@ return {
     config = function()
         local cmp = require 'cmp'
         local luasnip = require 'luasnip'
-        require("luasnip.loaders.from_vscode").lazy_load { exclude = { 'rust' } }
+        require("luasnip.loaders.from_vscode").lazy_load { exclude = { 'rust', 'sql' } }
         luasnip.config.setup { update_events = 'TextChanged,TextChangedI' }
 
         cmp.setup {
