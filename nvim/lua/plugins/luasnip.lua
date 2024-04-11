b0eafcf4badc27335f9746e5fe5a9d404c4f3d25
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-12 02:08:06 +0800

    Create snippets for nginx configuration

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -19,6 +19,7 @@ return {
         local format = require 'luasnip.extras.fmt'
         luasnip.add_snippets('all', require('snippets.all')(luasnip, format, extras))
         luasnip.add_snippets('yaml', require('snippets.yaml')(luasnip, format, extras))
+        luasnip.add_snippets('nginx', require('snippets.nginx')(luasnip, format))
         luasnip.add_snippets('sql', require('snippets.sql')(luasnip, format, extras))
         luasnip.add_snippets('lua', require('snippets.lua')(luasnip, format, extras))
         luasnip.add_snippets('cs', require('snippets.csharp')(luasnip, format, extras))
---
+++ nvim/lua/snippets/nginx.lua
@@ -0,0 +1,74 @@
+return function(luasnip, format)
+    local fmt = format.fmt
+    local t = luasnip.text_node
+    local i = luasnip.insert_node
+    local c = luasnip.choice_node
+    local snippet = luasnip.snippet
+    return {
+        snippet('worker_processes', fmt(
+            [[
+                worker_processes {};
+            ]], { c(1, { i(1, '1'), i(1, 'auto') }) }
+        )),
+        snippet('include', fmt(
+            [[include  {file};]], { file = i(1, 'file') }
+
+        )),
+        snippet('events', fmt(
+            [[
+                events {{
+                    {directive};
+                }}
+            ]], { directive = i(1, 'directive') }
+        )),
+        snippet('http', fmt(
+            [[
+                http {{
+                    {directive};
+                }}
+            ]], { directive = i(1, 'directive') }
+        )),
+        snippet('server', fmt(
+            [[
+                server {{
+                    listen  {port};
+                }}
+            ]], { port = i(1, '80') }
+        )),
+        snippet('location', fmt(
+            [[
+                location {location} {{
+                    proxy_pass  http://127.0.0.1:{port};
+                }}
+            ]], { location = i(1, '/'), port = i(2, '8080') }
+        )),
+        snippet('upstream', fmt(
+            [[
+                upstream {upstream} {{
+                    server {server};
+                }}
+            ]], { upstream = i(1, 'big_server_com'), server = i(2, '127.0.0.1') }
+        )),
+        snippet('localhost', t('127.0.0.1')),
+        snippet('server_name', fmt(
+            [[
+                server_name {server_name};
+            ]], { server_name = i(1, 'big.server.com') }
+        )),
+        snippet('access_log', fmt(
+            [[
+                access_log  {logfile};
+            ]], { logfile = i(1, 'logs/access.log') }
+        )),
+        snippet('error_log', fmt(
+            [[
+                error_log  {logfile};
+            ]], { logfile = i(1, 'logs/error.log') }
+        )),
+        snippet('listen', fmt(
+            [[
+                listen  {port};
+            ]], { port = i(1, '80') }
+        )),
+    }
+end
