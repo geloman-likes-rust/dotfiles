93db10902d51390d41c6f8c5165bbfd573c717f4
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-26 18:26:37 +0800

    Remove nvim plugins that i no longer need

--- nvim/lua/plugins/nvim-docker.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'dgrbrady/nvim-docker',
-    keys = { { "<leader>C", desc = "List Docker Containers" } },
-    config = function()
-        vim.keymap.set("n", "<leader>C", require("nvim-docker").containers.list_containers, {})
-    end,
-    dependencies = { 'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim', 'pvsfair/reactivex.nvim' },
-}
