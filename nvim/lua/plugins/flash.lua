902941d25b9b0016d817fbccd2de09d62c21954e
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-16 02:46:29 +0800

    Update flash configs

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,6 +1,5 @@
 return {
     "folke/flash.nvim",
-    opts = {},
     keys = function()
         local flash = require 'flash'
         local toggle = function() flash.toggle() end
@@ -9,5 +8,9 @@ return {
             { "<leader>s",  jump,   desc = "Flash Jump" },
             { "<leader>tf", toggle, desc = "Flash Toggle" },
         }
-    end
+    end,
+    opts = {
+        label = { rainbow = { enabled = true } },
+        modes = { char = { enabled = false } }
+    },
 }
