2db921d3d62f31c442dc28ceb0db52f0823f8a97
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-16 01:57:44 +0800

    Update autotag configs

--- nvim/lua/plugins/autotag.lua
+++ nvim/lua/plugins/autotag.lua
@@ -1,6 +1,19 @@
 return {
     "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
+    ft = {
+        "astro", "glimmer", "handlebars",
+        "javascript", "jsx", "markdown",
+        "rescript", "svelte", "tsx", "php",
+        "typescript", "vue", "xml", "html"
+    },
     opts = { autotag = { enable = true } },
-    config = function(_, opts) require('nvim-ts-autotag').setup(opts) end
+    config = function(_, opts)
+        ---@diagnostic disable-next-line: missing-fields
+        require 'nvim-treesitter.configs'.setup {
+            autotag = {
+                enable = true,
+            }
+        }
+        require('nvim-ts-autotag').setup(opts)
+    end
 }
