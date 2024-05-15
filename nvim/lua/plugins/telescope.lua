289a52ffe437fc03220717858cb37311bdb6f0e0
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-16 02:11:16 +0800

    Update telescope configs

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     version = '*',
-    cmd = "Telescope",
+    cmd = 'Telescope',
     init = function()
         vim.keymap.set('n', '<leader>cn', function()
             require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' }
@@ -12,47 +12,47 @@ return {
         return {
 
             -- RESUME CURRENT SEARCH
-            { "<leader>rs",      cmd "Telescope resume",         desc = "Telescope resume" },
+            { '<leader>rs',      cmd 'Telescope resume',         desc = 'Telescope resume' },
 
             -- BUFFERS, FILES, JUMPLIST, AND REGISTERS
-            { "<leader>ff",      cmd "Telescope find_files",     desc = "[F]ind [F]iles" },
-            { "<leader>fr",      cmd "Telescope registers",      desc = "[F]ind [R]egisters" },
-            { "<leader><space>", cmd "Telescope buffers",        desc = "[ ] Find existing buffers" },
-            { "<leader>jl",      cmd "Telescope jumplist",       desc = "[J]ump [L]ist" },
+            { '<leader>ff',      cmd 'Telescope find_files',     desc = '[F]ind [F]iles' },
+            { '<leader>fr',      cmd 'Telescope registers',      desc = '[F]ind [R]egisters' },
+            { '<leader><space>', cmd 'Telescope buffers',        desc = '[ ] Find existing buffers' },
+            { '<leader>jl',      cmd 'Telescope jumplist',       desc = '[J]ump [L]ist' },
 
             -- REGULAR EXPRESSIONS
-            { "<leader>lg",      cmd "Telescope live_grep",      desc = "[L]ive [G]rep" },
-            { "<leader>fw",      cmd "Telescope grep_string",    desc = "[F]ind [W]ord under cursor" },
+            { '<leader>lg',      cmd 'Telescope live_grep',      desc = '[L]ive [G]rep' },
+            { '<leader>fw',      cmd 'Telescope grep_string',    desc = '[F]ind [W]ord under cursor' },
 
             -- LSP RELATED
-            { "<leader>lr",      cmd "Telescope lsp_references", desc = "[L]sp [R]eferences" },
-            { "<leader>wd",      cmd "Telescope diagnostics",    desc = "[W]orkspace [D]iagnostics" },
+            { '<leader>lr',      cmd 'Telescope lsp_references', desc = '[L]sp [R]eferences' },
+            { '<leader>wd',      cmd 'Telescope diagnostics',    desc = '[W]orkspace [D]iagnostics' },
 
             -- COLORSCHEMES AND HIGHLIGHTS
-            { "<leader>hp",      cmd "Telescope highlights",     desc = "[H]ighlights [P]review" },
-            { "<leader>cc",      cmd "Telescope colorscheme",    desc = "[C]hange [C]olorscheme" },
+            { '<leader>hp',      cmd 'Telescope highlights',     desc = '[H]ighlights [P]review' },
+            { '<leader>cc',      cmd 'Telescope colorscheme',    desc = '[C]hange [C]olorscheme' },
 
             -- GIT COMMANDS
-            { "<leader>gs",      cmd "Telescope git_status",     desc = "[G]it [S]tatus" },
-            { "<leader>gf",      cmd "Telescope git_files",      desc = "[G]it [F]iles" },
-            { "<leader>gts",     cmd "Telescope git_stash",      desc = "Git Stash" },
-            { "<leader>gtb",     cmd "Telescope git_bcommits",   desc = "Git Current Buffer Commits" },
+            { '<leader>gs',      cmd 'Telescope git_status',     desc = '[G]it [S]tatus' },
+            { '<leader>gf',      cmd 'Telescope git_files',      desc = '[G]it [F]iles' },
+            { '<leader>gts',     cmd 'Telescope git_stash',      desc = 'Git Stash' },
+            { '<leader>gtb',     cmd 'Telescope git_bcommits',   desc = 'Git Current Buffer Commits' },
 
             -- TELESCOPE'S EASTER EGG
-            { "<leader>ps",      cmd "Telescope planets",        desc = "Love and Peace" },
+            { '<leader>ps',      cmd 'Telescope planets',        desc = 'Love and Peace' },
 
             -- HELP TAGS
-            { "<leader>hh",      cmd "Telescope help_tags",      desc = "Search Help" },
+            { '<leader>hh',      cmd 'Telescope help_tags',      desc = 'Search Help' },
 
         }
     end,
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = function()
-        local ignore_files = { ".git", "target", "node_modules", "wwwroot/lib", "**/Debug", "**.cache", "**/assets",
-            "**.png",
-            "**.svg", "**.favicon.*", "**.gif", "**.jpg", "**.jpeg", "**.mp4", "**.mp3", "**.pdf", "go", "**.zip",
-            "**.tar.gz",
-            "**/tags", "**.ttf" }
+        local ignore_files = {
+            '.git', '**.png', '**.svg', '**.favicon.*', '**.gif',
+            '**.jpg', '**.jpeg', '**.mp4', '**.mp3', '**.pdf',
+            'go', '**.zip', '**.tar.gz', '**/tags', '**.ttf'
+        }
 
         local actions = require('telescope.actions')
         local options = {
@@ -66,7 +66,7 @@ return {
                 },
 
                 jumplist = {
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     layout_config = {
                         preview_width = 0.55
                     },
@@ -85,7 +85,7 @@ return {
                 },
 
                 lsp_references = {
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     layout_config = {
                         preview_width = 0.60,
                     },
@@ -94,14 +94,14 @@ return {
                 planets = {
                     show_pluto = true,
                     show_moon = true,
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     layout_config = {
                         preview_width = 0.55
                     },
                 },
 
                 colorscheme = {
-                    initial_mode = "insert",
+                    initial_mode = 'insert',
                     layout_config = {
                         width = 0.40,
                         height = 0.60,
@@ -109,29 +109,29 @@ return {
                 },
 
                 grep_string = {
-                    word_match = "-w",
-                    initial_mode = "normal",
+                    word_match = '-w',
+                    initial_mode = 'normal',
                     layout_config = {
                         preview_width = 0.55
                     },
                 },
 
                 oldfiles = {
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     layout_config = {
                         preview_width = 0.55
                     },
                 },
 
                 diagnostics = {
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     layout_config = {
                         preview_width = 0.55
                     },
                 },
 
                 buffers = {
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     mappings = {
                         n = {
                             ['dd'] = actions.delete_buffer,
@@ -150,7 +150,7 @@ return {
                 },
 
                 git_status = {
-                    initial_mode = "normal",
+                    initial_mode = 'normal',
                     show_untracked = true,
                     layout_config = {
                         preview_width = 0.55
@@ -177,7 +177,7 @@ return {
                     n = {
                         ['<C-k>'] = actions.move_selection_previous,
                         ['<C-j>'] = actions.move_selection_next,
-                        ["q"] = actions.close
+                        ['q'] = actions.close
                     }
                 },
 
@@ -186,9 +186,9 @@ return {
                     width = 0.95,
                 },
 
-                -- selection_caret = "  ",
-                selection_caret = "  ",
-                prompt_prefix = "   ",
+                -- selection_caret = '  ',
+                selection_caret = '  ',
+                prompt_prefix = '   ',
             },
         }
         return options
