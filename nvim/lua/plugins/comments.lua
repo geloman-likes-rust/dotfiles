23d57ebca8a9e33cfd2bfc3af5f19add9ff09369
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-16 01:58:02 +0800

    Update comments plugin configs

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,5 +1,11 @@
 return {
     "numToStr/Comment.nvim",
-    event = "CursorMoved",
+    keys = function()
+        local comment = require('Comment.api')
+        return {
+            { mode = 'v', 'gc',  function() comment.toggle.linewise() end },
+            { mode = 'n', 'gcc', function() comment.toggle.linewise.current() end }
+        }
+    end,
     opts = {}
 }
