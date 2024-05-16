3f47f2317b1761f6a5fefac911335672a95042cf
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-17 00:06:59 +0800

    Update fugitive config

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,13 +1,13 @@
 return {
     'tpope/vim-fugitive',
-    cmd = { "Git", "Gvdiffsplit" },
+    cmd = { 'Git', 'Gvdiffsplit' },
     dependencies = { 'tpope/vim-rhubarb' },
     keys = function()
         return {
-            { "<leader>gc",  function() vim.cmd("Git commit") end,   desc = "[G]it [C]ommit" },
-            { "<leader>gp",  function() vim.cmd("Git push") end,     desc = "[G]it [P]ush" },
-            { "<leader>G",   function() vim.cmd("vertical Git") end, desc = "[G]it fugitive" },
-            { "<leader>gds", function() vim.cmd("Gvdiffsplit") end,  desc = "[G]it [D]iff [S]plit" },
+            { '<leader>gc',  '<cmd>Git commit<cr>',   desc = '[G]it [C]ommit' },
+            { '<leader>gp',  '<cmd>Git push<cr>',     desc = '[G]it [P]ush' },
+            { '<leader>G',   '<cmd>vertical Git<cr>', desc = '[G]it fugitive' },
+            { '<leader>gds', '<cmd>Gvdiffsplit<cr>',  desc = '[G]it [D]iff [S]plit' },
         }
     end,
 }
