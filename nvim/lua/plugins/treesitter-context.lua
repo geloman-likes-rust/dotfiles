587d5d66a61c5784f684d18c5a8f6db87afe7ee1
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-17 00:08:13 +0800

    Update treesitter-context config

--- nvim/lua/plugins/treesitter-context.lua
+++ nvim/lua/plugins/treesitter-context.lua
@@ -2,11 +2,11 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     cmd = { 'TSContextEnable', 'TSContextDisable', 'TSContextToggle' },
     init = function()
-        vim.keymap.set('n', '<leader>te', function() vim.cmd('TSContextEnable') end, { desc = 'Enable TSContext' })
-        vim.keymap.set('n', '<leader>td', function() vim.cmd('TSContextDisable') end, { desc = 'Disable TSContext' })
-        vim.keymap.set('n', '<leader>tt', function() vim.cmd('TSContextToggle') end, { desc = 'Toggle TSContext' })
+        vim.keymap.set('n', '<leader>te', '<cmd>TSContextEnable<cr>', { desc = 'Enable TSContext' })
+        vim.keymap.set('n', '<leader>td', '<cmd>TSContextDisable<cr>', { desc = 'Disable TSContext' })
+        vim.keymap.set('n', '<leader>tt', '<cmd>TSContextToggle<cr>', { desc = 'Toggle TSContext' })
     end,
     opts = {
-        max_lines = 4, -- prevent from taking over my entire screen.
+        max_lines = 5, -- prevent from taking over my entire screen.
     }
 }
