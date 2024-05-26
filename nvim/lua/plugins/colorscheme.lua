1878b1ce00f1cea876194d691823dc5f09f7f36d
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-26 08:30:31 +0800

    Add new colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,69 @@
 return {
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    opts = {
-        style = 'deep',
-        transparent = true,
-        term_colors = true,
-        code_style = {
-            comments = 'italic,bold',
-            keywords = 'none',
-            functions = 'none',
-            strings = 'none',
-            variables = 'none'
+    {
+        'navarasu/onedark.nvim',
+        priority = 1000,
+        opts = {
+            style = 'deep',
+            transparent = true,
+            term_colors = true,
+            code_style = {
+                comments = 'italic,bold',
+                keywords = 'none',
+                functions = 'none',
+                strings = 'none',
+                variables = 'none'
+            },
         },
+        config = function(_, opts)
+            require('onedark').setup(opts)
+            require('onedark').load()
+        end
     },
-    config = function(_, opts)
-        require('onedark').setup(opts)
-        require('onedark').load()
-    end
+    {
+        'rebelot/kanagawa.nvim',
+        lazy = true,
+        priority = 1000,
+        transparent = true,
+        opts = {
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none"
+                        }
+                    }
+                }
+            }
+        },
+        config = function(_, opts)
+            require('kanagawa').setup(opts)
+            require('kanagawa').load 'dragon'
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin",
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+            require('catppuccin').setup(opts)
+        end
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {
+        },
+        config = function(_, opts)
+            require('gruvbox').setup(opts)
+        end
+    }
 }
