9893b1feb1e7da3ed05e540c6a3e65ed430b5527
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-27 23:00:53 +0800

    Rename autocmd.lua --> autocmds.lua

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 require 'config.lazy'
-require 'config.autocmd'
+require 'config.autocmds'
 require 'config.options'
 require 'config.keymaps'
 require 'config.highlights'
