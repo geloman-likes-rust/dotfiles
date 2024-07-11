b238200fe3a85ae253e4f65eccafe4bb9fdf8aeb
geloman <geloman-likes-rust@users.noreply.github.com>
2024-07-11 12:31:32 +0800

    Create keymap for toggling quickfix list

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,6 +3,7 @@ return {
     cmd = 'Trouble',
     keys = {
         { '<leader>tw', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Toggle Workspace Diagnostics' },
+        { '<leader>tq', '<cmd>Trouble quickfix<cr>',           desc = 'Toggle quickfix list' },
         { '<leader>tr', '<cmd>Trouble lsp toggle<cr>',         desc = 'Toggle Lsp References' },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
