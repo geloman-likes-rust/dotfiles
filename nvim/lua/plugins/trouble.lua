618d796ed93fe0926b4cb45c1d22b69f49bb42b2
geloman <geloman-likes-rust@users.noreply.github.com>
2024-07-11 12:32:41 +0800

    Update trouble config

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,9 +2,9 @@ return {
     'folke/trouble.nvim',
     cmd = 'Trouble',
     keys = {
-        { '<leader>tw', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Toggle Workspace Diagnostics' },
         { '<leader>tq', '<cmd>Trouble quickfix<cr>',           desc = 'Toggle quickfix list' },
         { '<leader>tr', '<cmd>Trouble lsp toggle<cr>',         desc = 'Toggle Lsp References' },
+        { '<leader>tw', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Toggle Workspace Diagnostics' },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = { focus = true },
