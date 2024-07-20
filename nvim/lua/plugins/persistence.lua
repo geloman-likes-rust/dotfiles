a0c0eae6b4818ed414971b5ff583e21011b370e7
geloman <geloman-likes-rust@users.noreply.github.com>
2024-07-20 12:44:10 +0800

    Add plugin session management

---
+++ nvim/lua/plugins/persistence.lua
@@ -0,0 +1,8 @@
+return {
+    'folke/persistence.nvim',
+    opts = {},
+    keys = {
+        { '<leader>rs', function() require('persistence').load() end,                desc = 'Restore Session' },
+        { '<leader>rl', function() require('persistence').load({ last = true }) end, desc = 'Restore Last Session' },
+    },
+}
