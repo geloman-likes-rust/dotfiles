995430309d8f1206d2b6dc4917677411c34f9c21
geloman <geloman-likes-rust@users.noreply.github.com>
2024-07-20 21:54:56 +0800

    Add plugin: nvim-ufo

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,6 +8,10 @@ return {
         local util = lspconfig.util
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
         capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
         --[[ LUA LSP ]]
