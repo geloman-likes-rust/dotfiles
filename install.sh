0d0f5aef770adb2f3d895b5622bded3b700214e3
geloman <geloman-likes-rust@users.noreply.github.com>
2024-07-30 08:04:02 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@
 install_tmux_tpm() {
 	[ -d ~/.tmux ] && return
 	echo "Installing tmux-plugins/tpm......................................"
+    mkdir -p ~/.tmux/plugins/tpm
 	git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    [ -n "$(command -v tmux)" ] && tmux -c "~/.tmux/plugins/tpm/tpm && ~/.tmux/plugins/tpm/bin/install_plugins"
 }
 
 ## FUZZY FINDER
