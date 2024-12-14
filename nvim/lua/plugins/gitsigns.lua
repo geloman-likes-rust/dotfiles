9bc7a2136d2242ab2b3aabd79d577aa92ce0dbb7
gelocraft <gelocraft@users.noreply.github.com>
2024-12-14 10:01:38 +0800

    nvim(plugin/gitsigns): update gutter signs

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,12 +3,22 @@ return {
 	ft = { 'gitcommit', 'diff' },
 	opts = {
 		signs = {
-			add = { text = '+' },
-			change = { text = '~' },
+			add = { text = '┃' },
+			change = { text = '┃' },
 			delete = { text = '_' },
 			topdelete = { text = '‾' },
 			changedelete = { text = '~' },
+			untracked = { text = '┆' },
 		},
+		signs_staged = {
+			add = { text = '┃' },
+			change = { text = '┃' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '~' },
+			untracked = { text = '┆' },
+		},
+		signs_staged_enable = true,
 		numhl = true,
 		current_line_blame_opts = {
 			virt_text = true,
